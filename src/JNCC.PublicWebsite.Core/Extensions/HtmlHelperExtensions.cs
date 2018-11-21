44834784d2fbecd56fa120029e576f8b675d71a4
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-20 19:18:11 -0600

    Added HtmlHelper for Directionality based on CultureInfo

---
+++ src/JNCC.PublicWebsite.Core/Extensions/HtmlHelperExtensions.cs
@@ -0,0 +1,19 @@
+﻿using JNCC.PublicWebsite.Core.Constants;
+using System.Globalization;
+using System.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Extensions
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string Directionality(this HtmlHelper htmlHelper, CultureInfo cultureInfo)
+        {
+            if (cultureInfo == null || cultureInfo.TextInfo == null)
+            {
+                return HtmlTextDirectionalities.Auto;
+            }
+
+            return cultureInfo.TextInfo.IsRightToLeft ? HtmlTextDirectionalities.RightToLeft : HtmlTextDirectionalities.LeftToRight;
+        }
+    }
+}
