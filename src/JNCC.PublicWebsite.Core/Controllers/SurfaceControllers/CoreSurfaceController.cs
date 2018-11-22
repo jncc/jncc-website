c94c4da7d49ea304df15731f9ca1f1d658e82bdd
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-22 14:36:32 -0600

    Added EmptyResult short hand function within CoreSurfaceController.

--- src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/CoreSurfaceController.cs
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/CoreSurfaceController.cs
@@ -1,5 +1,6 @@
 ﻿using JNCC.PublicWebsite.Core.Extensions;
 using JNCC.PublicWebsite.Core.Models;
+using System.Web.Mvc;
 using System.Web.Routing;
 using Umbraco.Web.Mvc;
 
@@ -7,12 +8,18 @@ namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
 {
     public abstract class CoreSurfaceController : SurfaceController
     {
+        private static readonly EmptyResult emptyResult = new EmptyResult();
         protected HomePage Root { get; private set; }
 
         protected override void Initialize(RequestContext requestContext)
         {
             base.Initialize(requestContext);
             Root = CurrentPage.Site<HomePage>();
+        }
+
+        protected EmptyResult EmptyResult()
+        {
+            return emptyResult;
         }
     }
 }
