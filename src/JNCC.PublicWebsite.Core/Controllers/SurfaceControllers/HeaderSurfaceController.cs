b4565b5805be21893c3d8ef56354aa84897d2894
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-22 12:40:06 -0600

    Added basic SurfaceController for Main Navigation in Header

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/HeaderSurfaceController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public class HeaderSurfaceController : SurfaceController
+    {
+        [ChildActionOnly]
+        public PartialViewResult RenderMainNavigation()
+        {
+            return PartialView("~/Views/Partials/Header/MainNavigation.cshtml");
+        }
+    }
+}
