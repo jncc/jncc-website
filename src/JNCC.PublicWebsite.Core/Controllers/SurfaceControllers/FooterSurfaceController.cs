53a42abe7e655e5983f1f23b7612d60b07db267b
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-22 13:25:38 -0600

    Added Footer Content with dynamic content

--- src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/FooterSurfaceController.cs
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/FooterSurfaceController.cs
@@ -1,10 +1,23 @@
-﻿using System.Web.Mvc;
+﻿using JNCC.PublicWebsite.Core.Models;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Umbraco.Web;
 using Umbraco.Web.Mvc;
 
 namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
 {
     public class FooterSurfaceController : SurfaceController
     {
+        private HomePage Root { get; set; }
+
+        protected override void Initialize(RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+            Root = CurrentPage.AncestorOrSelf<HomePage>();
+        }
+
+
         [ChildActionOnly]
         public PartialViewResult RenderCategorisedLinks()
         {
@@ -15,6 +28,12 @@ namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
         public PartialViewResult RenderSocialMediaLinks()
         {
             return PartialView("~/Views/Partials/Footer/SocialMediaLinks.cshtml");
+        }
+
+        [ChildActionOnly]
+        public IHtmlString RenderContent()
+        {
+            return Root.FooterContent;
         }
     }
 }
