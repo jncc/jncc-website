3cf70b12259dc753e9b44fb3b64fcaa487d0b6de
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-22 14:30:00 -0600

    Added view models for Main Navigation code.

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/MainNavigationItemViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class MainNavigationItemViewModel : NavigationItemViewModel
+    {
+        public bool IsActive { get; set; }
+        public IEnumerable<MainNavigationItemViewModel> Children { get; set; }
+    }
+}
