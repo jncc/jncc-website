02d8219d92c73dfac7cc86efa4b2501c63c9174c
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-22 14:27:25 -0600

    Added IPublishedContent extension methods.

---
+++ src/JNCC.PublicWebsite.Core/Extensions/PublishedContentExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace JNCC.PublicWebsite.Core.Extensions
+{
+    public static class PublishedContentExtensions
+    {
+        public static bool AreChildrenVisible(this IPublishedContent content)
+        {
+            return content.GetPropertyValue<bool>("umbracoNaviHideChildren") == false;
+        }
+
+        public static T Site<T>(this IPublishedContent content) where T : class, IPublishedContent
+        {
+            var site = content.Site();
+
+            return site as T;
+        }
+
+        public static IEnumerable<IPublishedContent> VisibleChildren(this IPublishedContent content)
+        {
+            return content.Children.Where(x => x.IsVisible());
+        }
+    }
+}
