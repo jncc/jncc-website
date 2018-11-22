7b9361850bdb8fdf84529c8a8272a377645f5b21
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-22 13:06:14 -0600

    Added Footer Categorised Links

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/FooterSurfaceController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public class FooterSurfaceController : SurfaceController
+    {
+        [ChildActionOnly]
+        public PartialViewResult RenderCategorisedLinks()
+        {
+            return PartialView("~/Views/Partials/Footer/CategorisedLinks.cshtml");
+        }
+    }
+}
