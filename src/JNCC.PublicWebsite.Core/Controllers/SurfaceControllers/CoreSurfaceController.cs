545b631e9520b3c6cc0745bc29183f176ee54358
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-22 14:11:42 -0600

    Added CoreSurfaceController which adds additional properties

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/CoreSurfaceController.cs
@@ -0,0 +1,18 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using System.Web.Routing;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public abstract class CoreSurfaceController : SurfaceController
+    {
+        protected HomePage Root { get; private set; }
+
+        protected override void Initialize(RequestContext requestContext)
+        {
+            base.Initialize(requestContext);
+            Root = CurrentPage.AncestorOrSelf<HomePage>();
+        }
+    }
+}
