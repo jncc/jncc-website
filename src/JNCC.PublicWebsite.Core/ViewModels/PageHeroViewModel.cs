3ecc093aacbf415601988e7ac972c3294d8bf324
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-23 16:23:10 -0600

    Added PageHeroViewModel

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/PageHeroViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class PageHeroViewModel
+    {
+        public string Headline { get; set; }
+        public string HeroImageUrl { get; set; }
+    }
+}
