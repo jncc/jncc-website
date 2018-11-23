a10ab249b7e16fbf1d83367dfaa89f1039c0ac18
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-23 15:03:08 -0600

    Wired up NavigationItemService to get Footer Uncategorised Links

--- src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/FooterSurfaceController.cs
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/FooterSurfaceController.cs
@@ -1,4 +1,4 @@
-﻿using JNCC.PublicWebsite.Core.Services;
+using JNCC.PublicWebsite.Core.Services;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -7,6 +7,7 @@ namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
 {
     public sealed class FooterSurfaceController : CoreSurfaceController
     {
+        private readonly NavigationItemService navigationItemService = new NavigationItemService();
         private readonly SocialMediaLinksService socialMediaLinksService = new SocialMediaLinksService();
         private readonly CategorisedFooterLinksService categorisedFooterLinksService = new CategorisedFooterLinksService();
 
@@ -43,9 +44,16 @@ namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
         }
 
         [ChildActionOnly]
-        public PartialViewResult RenderUncategorisedLinks()
+        public ActionResult RenderUncategorisedLinks()
         {
-            return PartialView("~/Views/Partials/Footer/UncategorisedLinks.cshtml");
+            var links = navigationItemService.GetViewModels(Root.FooterUncategorisedLinks);
+
+            if (links == null || links.Any() == false)
+            {
+                return EmptyResult();
+            }
+
+            return PartialView("~/Views/Partials/Footer/UncategorisedLinks.cshtml", links);
         }
     }
 }
