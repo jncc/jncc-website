ec292bc3256e6b0595c2ed3e95d20b8fb86f6326
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-23 16:20:54 -0600

    Wired up PageHeroSurfaceController with static view

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/PageHeroSurfaceController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public sealed class PageHeroSurfaceController : CoreSurfaceController
+    {
+        [ChildActionOnly]
+        public ActionResult RenderPageHero()
+        {
+            return PartialView("~/Views/Partials/PageHero.cshtml");
+        }
+    }
+}
