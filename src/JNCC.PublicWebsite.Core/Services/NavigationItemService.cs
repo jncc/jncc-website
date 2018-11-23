f31df52f3df23a514813488a94fa09b7c20ca737
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-23 15:02:03 -0600

    Added NavigationItemService class.

---
+++ src/JNCC.PublicWebsite.Core/Services/NavigationItemService.cs
@@ -0,0 +1,33 @@
+﻿using JNCC.PublicWebsite.Core.ViewModels;
+using RJP.MultiUrlPicker.Models;
+using System.Collections.Generic;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    public sealed class NavigationItemService
+    {
+        public IEnumerable<NavigationItemViewModel> GetViewModels(IEnumerable<Link> links)
+        {
+            return GetViewModels<NavigationItemViewModel>(links);
+        }
+
+        public IEnumerable<T> GetViewModels<T>(IEnumerable<Link> links) where T : NavigationItemViewModel, new()
+        {
+            var viewModels = new List<T>();
+
+            foreach (var link in links)
+            {
+                var viewModel = new T()
+                {
+                    Url = link.Url,
+                    Text = link.Name,
+                    Target = link.Target
+                };
+
+                viewModels.Add(viewModel);
+            }
+
+            return viewModels;
+        }
+    }
+}
