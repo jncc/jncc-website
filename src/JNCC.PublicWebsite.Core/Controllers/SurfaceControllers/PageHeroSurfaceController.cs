f05e3b3c24028292121bfd8d6c0855f60ce643d2
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-23 17:13:35 -0600

    Wired up Page Hero surface controller with service to get view model

--- src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/PageHeroSurfaceController.cs
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/PageHeroSurfaceController.cs
@@ -1,13 +1,29 @@
-﻿using System.Web.Mvc;
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Services;
+using System.Web.Mvc;
 
 namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
 {
     public sealed class PageHeroSurfaceController : CoreSurfaceController
     {
+        private readonly PageHeroService _pageHeroService = new PageHeroService();
+
         [ChildActionOnly]
         public ActionResult RenderPageHero()
         {
-            return PartialView("~/Views/Partials/PageHero.cshtml");
+            if (CurrentPage is IPageHeroComposition == false)
+            {
+                return EmptyResult();
+            }
+
+            var viewModel = _pageHeroService.GetViewModel(CurrentPage as IPageHeroComposition);
+
+            if (viewModel == null)
+            {
+                return EmptyResult();
+            }
+
+            return PartialView("~/Views/Partials/PageHero.cshtml", viewModel);
         }
     }
 }
