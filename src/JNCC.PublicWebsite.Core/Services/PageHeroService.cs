41c24e872a1d032d6738abeea187027b418f465f
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-23 16:57:57 -0600

    PageHeroService now converts IPageHeroComposition to PageHeroViewModel

---
+++ src/JNCC.PublicWebsite.Core/Services/PageHeroService.cs
@@ -0,0 +1,26 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.ViewModels;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal sealed class PageHeroService
+    {
+        public PageHeroViewModel GetViewModel(IPageHeroComposition pageHeroComposition)
+        {
+            if (pageHeroComposition.HeroImage == null)
+            {
+                return null;
+            }
+
+            var headline = string.IsNullOrWhiteSpace(pageHeroComposition.Headline) == false ? 
+                             pageHeroComposition.Headline 
+                           : pageHeroComposition.Name;
+
+            return new PageHeroViewModel()
+            {
+                Headline = headline,
+                HeroImageUrl = pageHeroComposition.HeroImage.Url
+            };
+        }
+    }
+}
