85bd348ff1b821bd3f25502e93bc841033a4438e
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-26 17:30:31 -0600

    Added wire up of SidebarService to SurfaceController

--- src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/SidebarSurfaceController.cs
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/SidebarSurfaceController.cs
@@ -1,10 +1,20 @@
 ﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Services;
 using System.Web.Mvc;
 
 namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
 {
     public sealed class SidebarSurfaceController : CoreSurfaceController
     {
+        private readonly NavigationItemService _navigationItemService;
+        private readonly SidebarService _sidebarService;
+
+        public SidebarSurfaceController()
+        {
+            _navigationItemService = new NavigationItemService();
+            _sidebarService = new SidebarService(_navigationItemService);
+        }
+
         [HttpGet]
         public ActionResult RenderSidebar()
         {
@@ -13,7 +23,9 @@ namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
                 return EmptyResult();
             }
 
-            return PartialView("~/Views/Partials/Sidebar.cshtml");
+            var viewModel = _sidebarService.GetViewModel(CurrentPage as ISidebarComposition);
+
+            return PartialView("~/Views/Partials/Sidebar.cshtml", viewModel);
         }
     }
 }
