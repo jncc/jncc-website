ee706951285cd1cd909831896f2eab5b95a6c5dc
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-26 16:38:39 -0600

    Added basic Sidebar surface controller.

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/SidebarSurfaceController.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public sealed class SidebarSurfaceController : CoreSurfaceController
+    {
+        [HttpGet]
+        public ActionResult RenderSidebar()
+        {
+            return PartialView("~/Views/Partials/Sidebar.cshtml");
+        }
+    }
+}
