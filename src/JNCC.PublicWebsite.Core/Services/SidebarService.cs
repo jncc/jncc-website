3241f44f40bb1dcbfdd978abaffee73dc9d1a70c
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-26 17:29:54 -0600

    Added SidebarService for wiring up a ISidebarComp to the new view model

---
+++ src/JNCC.PublicWebsite.Core/Services/SidebarService.cs
@@ -0,0 +1,42 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.ViewModels;
+using System.Collections.Generic;
+using Umbraco.Web;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal sealed class SidebarService
+    {
+        private readonly NavigationItemService _navigationItemService;
+        private const int _sectionRootLevel = 3;
+
+        public SidebarService(NavigationItemService navigationItemService)
+        {
+            _navigationItemService = navigationItemService;
+        }
+
+        public SidebarViewModel GetViewModel(ISidebarComposition composition)
+        {
+            var viewModel = new SidebarViewModel()
+            {
+                GetInTouchButton = _navigationItemService.GetViewModel(composition.SidebarGetInTouchButton),
+                InThisSectionLinks = GetInThisSectionLinks(composition),
+                SeeAlsoLinks = _navigationItemService.GetViewModels(composition.SidebarSeeAlsoLinks)
+            };
+
+            return viewModel;
+        }
+
+        private IEnumerable<NavigationItemViewModel> GetInThisSectionLinks(ISidebarComposition composition)
+        {
+            var sectionRoot = composition.AncestorOrSelf(_sectionRootLevel);
+
+            if (sectionRoot == null)
+            {
+                return null;
+            }
+
+            return _navigationItemService.GetViewModels(sectionRoot.Children);
+        }
+    }
+}
