abfa2990a09594036f974eeaf592998229387698
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-26 17:28:12 -0600

    Added SidebarViewModel

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/SidebarViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public class SidebarViewModel
+    {
+        public NavigationItemViewModel GetInTouchButton { get; set; }
+
+        public IEnumerable<NavigationItemViewModel> InThisSectionLinks { get; set; }
+
+        public IEnumerable<NavigationItemViewModel> SeeAlsoLinks { get; set; }
+    }
+}
