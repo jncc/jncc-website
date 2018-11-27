d3086432ff78059b0175a9a21c2a1f17e515d9ad
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-27 15:45:02 -0600

    Wire up of service to surfacecontroller for Related Items

--- src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/RelatedItemsSurfaceController.cs
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/RelatedItemsSurfaceController.cs
@@ -1,13 +1,37 @@
-﻿using System.Web.Mvc;
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Services;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
 {
     public sealed class RelatedItemsSurfaceController : CoreSurfaceController
     {
+        private readonly NavigationItemService _navigationItemService;
+        private readonly RelatedItemsService _relatedItemsService;
+
+        public RelatedItemsSurfaceController()
+        {
+            _navigationItemService = new NavigationItemService();
+            _relatedItemsService = new RelatedItemsService(_navigationItemService);
+        }
+
         [ChildActionOnly]
         public ActionResult RenderRelatedItems()
         {
-            return PartialView("~/Views/Partials/RelatedItems.cshtml");
+            if (CurrentPage is IRelatedItemsComposition == false)
+            {
+                return EmptyResult();
+            }
+
+            var viewModels = _relatedItemsService.GetViewModels(CurrentPage as IRelatedItemsComposition);
+
+            if (viewModels == null || viewModels.Any() == false)
+            {
+                return EmptyResult();
+            }
+
+            return PartialView("~/Views/Partials/RelatedItems.cshtml", viewModels);
         }
     }
 }
