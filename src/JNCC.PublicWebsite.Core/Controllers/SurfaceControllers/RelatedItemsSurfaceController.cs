665f4f71c556da4df33848a8ad3b5cddb4932d5f
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-27 08:55:07 -0600

    Replaced static RelatedItems with static output SurfaceController

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/RelatedItemsSurfaceController.cs
@@ -0,0 +1,14 @@
+﻿using System.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public sealed class RelatedItemsSurfaceController : CoreSurfaceController
+    {
+        [HttpGet]
+        [ChildActionOnly]
+        public ActionResult RenderRelatedItems()
+        {
+            return PartialView("~/Views/Partials/RelatedItems.cshtml");
+        }
+    }
+}
