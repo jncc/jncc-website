47c9d1d8a5a194290f7e748fa4e1926cb36ebe12
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-30 15:40:28 -0500

    Added StaffDirectoryProfileViewModel

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/StaffDirectoryProfileViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class StaffDirectoryProfileViewModel
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string JobTitle { get; set; }
+        public IHtmlString Description { get; set; }
+        public string Url { get; set; }
+    }
+}
