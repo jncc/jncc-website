e92e315855d578cdb250b30c5ced64c457582155
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-30 14:24:37 -0500

    Added stub actions & static views for StaffDirectorySurfaceController

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/StaffDirectorySurfaceController.cs
@@ -0,0 +1,31 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public sealed class StaffDirectorySurfaceController : CoreSurfaceController
+    {
+        [ChildActionOnly]
+        public ActionResult RenderFiltering()
+        {
+            if (CurrentPage is StaffDirectoryPage == false)
+            {
+                return EmptyResult();
+            }
+
+            return PartialView("~/Views/Partials/StaffDirectory/Filtering.cshtml");
+        }
+
+        [ChildActionOnly]
+        public ActionResult RenderListing()
+        {
+            if (CurrentPage is StaffDirectoryPage == false)
+            {
+                return EmptyResult();
+            }
+
+            return PartialView("~/Views/Partials/StaffDirectory/Listing.cshtml");
+        }
+    }
+}
