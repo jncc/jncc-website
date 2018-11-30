97930fafc5fe0a674e7b92a5e18010f3aa7af35e
jamiepollock <jamie.does.umbraco@gmail.com>
2018-11-30 17:09:12 -0500

    Added StaffDirectoryService to get view models & pagination

---
+++ src/JNCC.PublicWebsite.Core/Services/StaffDirectoryService.cs
@@ -0,0 +1,43 @@
+﻿using JNCC.PublicWebsite.Core.Constants;
+using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.ViewModels;
+using System.Linq;
+using Umbraco.Core.Models;
+using Umbraco.Core.Persistence;
+using Umbraco.Web;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    public sealed class StaffDirectoryService
+    {
+        public Paged​Result<StaffDirectoryProfileViewModel> GetViewModels(StaffDirectoryPage staffDirectoryPage, int pageNumber)
+        {
+            var profilesPerPage = decimal.ToInt32(staffDirectoryPage.ProfilesPerPage);
+
+            var children = staffDirectoryPage.Children<StaffProfilePage>();
+            var results = new Paged​Result<StaffDirectoryProfileViewModel>(children.LongCount(), pageNumber, profilesPerPage);
+            var viewModels = children.Skip(results.GetSkipSize()).Take(profilesPerPage).Select(ToViewModel);
+            results.Items = viewModels;
+
+            return results;
+        }
+
+        private StaffDirectoryProfileViewModel ToViewModel(StaffProfilePage content)
+        {
+            var viewModel = new StaffDirectoryProfileViewModel
+            {
+                Name = string.IsNullOrWhiteSpace(content.FullName) ? content.Name : content.FullName,
+                JobTitle = content.JobTitle,
+                Description = content.DirectoryListingContent,
+                Url = content.Url
+            };
+
+            if (content.ProfilePicture != null)
+            {
+                viewModel.ImageUrl = content.ProfilePicture.GetCropUrl(ImageCropAliases.ListingThumbnail);
+            }
+
+            return viewModel;
+        }
+    }
+}
