16eb613eee346b6ddf83e5f379e293b8152ee5cc
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-03 18:14:34 -0500

    Added News & Insights Landing Page surface controller & partial views

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/NewsAndInsightsLandingSurfaceController.cs
@@ -0,0 +1,36 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Services;
+using System.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public sealed class NewsAndInsightsLandingSurfaceController : CoreSurfaceController
+    {
+        private readonly NewsAndInsightsLandingService _newsAndInsightsLandingService = new NewsAndInsightsLandingService();
+
+        [ChildActionOnly]
+        public ActionResult RenderFiltering()
+        {
+            if (CurrentPage is NewsAndInsightsLandingPage == false)
+            {
+                return EmptyResult();
+            }
+
+            return PartialView("~/Views/Partials/NewsAndInsightsLanding/Filtering.cshtml");
+        }
+
+        [HttpGet]
+        [ChildActionOnly]
+        public ActionResult RenderListing(int pageNumber = 1)
+        {
+            if (CurrentPage is NewsAndInsightsLandingPage == false)
+            {
+                return EmptyResult();
+            }
+
+            var results = _newsAndInsightsLandingService.GetViewModels(CurrentPage as NewsAndInsightsLandingPage, pageNumber);
+
+            return PartialView("~/Views/Partials/NewsAndInsightsLanding/Listing.cshtml", results);
+        }
+    }
+}
