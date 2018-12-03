eb99cf7c34bfefbc40c3ac2e5adee0db19b21efa
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-03 17:40:31 -0500

    Added SEO Meta Data Service

---
+++ src/JNCC.PublicWebsite.Core/Services/SeoMetaDataService.cs
@@ -0,0 +1,35 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.ViewModels;
+using Umbraco.Core.Models;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal sealed class SeoMetaDataService
+    {
+        public SeoMetaDataViewModel GetViewModel(IPublishedContent content)
+        {
+            return new SeoMetaDataViewModel
+            {
+                Title = content.Name
+            };
+        }
+
+
+        public SeoMetaDataViewModel GetViewModelFromSeoSettings(ISeoComposition composition)
+        {
+            var settings = composition.SeoSettings;
+
+            if (settings == null)
+            {
+                return GetViewModel(composition);
+            }
+
+            return new SeoMetaDataViewModel
+            {
+                Title = settings.Title,
+                Description = settings.Description,
+                Keywords = settings.Keywords
+            };
+        }
+    }
+}
