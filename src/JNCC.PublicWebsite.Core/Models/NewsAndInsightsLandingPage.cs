492135252cefd910e58813b02db4d30f623e07df
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-03 18:10:13 -0500

    Added Articles Per Page and Preamble to News & Insights Landing Page

---
+++ src/JNCC.PublicWebsite.Core/Models/NewsAndInsightsLandingPage.cs
@@ -0,0 +1,17 @@
+﻿using Umbraco.ModelsBuilder;
+using Umbraco.Web;
+
+namespace JNCC.PublicWebsite.Core.Models
+{
+    public partial class NewsAndInsightsLandingPage
+    {
+        ///<summary>
+        /// Articles Per Page: Determines how many articles should be shown per page by default.
+        ///</summary>
+        [ImplementPropertyType("articlesPerPage")]
+        public int ArticlesPerPage
+        {
+            get { return this.GetPropertyValue<int>("articlesPerPage"); }
+        }
+    }
+}
