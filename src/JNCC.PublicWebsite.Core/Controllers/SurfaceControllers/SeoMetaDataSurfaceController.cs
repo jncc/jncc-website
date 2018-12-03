612b3e530a847894568041767aed0f06687e9769
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-03 17:41:13 -0500

    Added Surface Controller and Partial View for SEO Meta Data.

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/SeoMetaDataSurfaceController.cs
@@ -0,0 +1,29 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Services;
+using JNCC.PublicWebsite.Core.ViewModels;
+using System.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public sealed class SeoMetaDataSurfaceController : CoreSurfaceController
+    {
+        private readonly SeoMetaDataService seoMetaDataService = new SeoMetaDataService();
+
+        [ChildActionOnly]
+        public ActionResult RenderSeoMetaData()
+        {
+            SeoMetaDataViewModel viewModel;
+
+            if (CurrentPage is ISeoComposition)
+            {
+                viewModel = seoMetaDataService.GetViewModelFromSeoSettings(CurrentPage as ISeoComposition);
+            }
+            else
+            {
+                viewModel = seoMetaDataService.GetViewModel(CurrentPage);
+            }
+
+            return PartialView("~/Views/Partials/SeoMetaData.cshtml", viewModel);
+        }
+    }
+}
