a2b912b0910abbf5bdf3226ff53f5719267c4aec
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-03 10:04:49 -0500

    Added ArticleListingViewModel

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/ArticleListingViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Web;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class ArticleListingViewModel
+    {
+        public string Title { get; set; }
+        public string ImageUrl { get; set; }
+        public DateTime PublishDate { get; set; }
+        public IHtmlString Description { get; set; }
+        public string Url { get; set; }
+    }
+}
