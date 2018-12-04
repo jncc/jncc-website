c399fa0c9cb4fc93ebed3ce29e08853b1c7bb613
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-04 17:42:01 -0500

    Added an extension for simplifying order by first available string

---
+++ src/JNCC.PublicWebsite.Core/Extensions/EnumerableExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JNCC.PublicWebsite.Core.Extensions
+{
+    public static class EnumerableExtensions
+    {
+        public static IOrderedEnumerable<T> OrderByFirstAvailableProperty<T>(this IEnumerable<T> enumerable, Func<T, IEnumerable<string>> properties)
+        {
+            return enumerable.OrderBy(x => properties.Invoke(x)
+                                                     .Where(y => string.IsNullOrWhiteSpace(y) == false)
+                                                     .FirstOrDefault()
+                                     );
+        }
+    }
+}
