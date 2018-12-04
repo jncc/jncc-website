c4181b3b22b315bcb4cadf78cac795056e6a4bbb
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-04 17:01:01 -0500

    Rewired StaffDirectoryPage ModelsBuilder decimal w/ int for ItemsPerPage

---
+++ src/JNCC.PublicWebsite.Core/Models/StaffDirectoryPage.cs
@@ -0,0 +1,17 @@
+﻿using Umbraco.ModelsBuilder;
+using Umbraco.Web;
+
+namespace JNCC.PublicWebsite.Core.Models
+{
+    public partial class StaffDirectoryPage
+    {
+        ///<summary>
+        /// Profiles Per Page: Determines how many profiles should be shown per page by default.
+        ///</summary>
+        [ImplementPropertyType("profilesPerPage")]
+        public int ProfilesPerPage
+        {
+            get { return this.GetPropertyValue<int>("profilesPerPage"); }
+        }
+    }
+}
