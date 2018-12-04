c2e66f821abdb5057053d1a69e76feff408e74a6
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-04 16:59:41 -0500

    Added abstract class ListingService to unify Staff Director & News logic

---
+++ src/JNCC.PublicWebsite.Core/Services/ListingService.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using Umbraco.Core.Models;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal abstract class ListingService<TParent, TChild, TViewModel> where TParent : IPublishedContent
+                                                                        where TChild : IPublishedContent
+    {
+        public Paged​Result<TViewModel> GetViewModels(TParent parent, int pageNumber)
+        {
+            var children = GetOrderedChildren(parent);
+            var numberOfItemsPerPage = GetItemsPerPage(parent);
+            var results = new Paged​Result<TViewModel>(children.LongCount(), pageNumber, numberOfItemsPerPage);
+
+            var viewModels = children.Skip(results.GetSkipSize())
+                                     .Take(numberOfItemsPerPage)
+                                     .Select(ToViewModel);
+
+            results.Items = viewModels;
+
+            return results;
+        }
+
+        protected abstract int GetItemsPerPage(TParent parent);
+
+        protected abstract IOrderedEnumerable<TChild> GetOrderedChildren(TParent parent);
+
+        protected abstract TViewModel ToViewModel(TChild content);
+    }
+}
