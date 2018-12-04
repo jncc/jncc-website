64b1c60470db1bf192dff3eb905ef8183b375f74
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-04 16:06:10 -0500

    Added SurfaceController & View for Rendering Cookie Banner Template.

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/CookieBannerSurfaceController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public sealed class CookieBannerTemplateSurfaceController : CoreSurfaceController
+    {
+        [ChildActionOnly]
+        public ActionResult RenderCookieBannerTemplate()
+        {
+            if (Root == null || Root.CookieBannerContent == null)
+            {
+                return EmptyResult();
+            }
+
+            return PartialView("~/Views/Partials/CookieBannerTemplate.cshtml", Root.CookieBannerContent);
+        }
+    }
+}
