a54f32a46e0f507ce4cf7992abc544144187d1d7
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-05 17:36:05 -0500

    Reworked Page Attributes into Surface Controller

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/PageAttributesSurfaceController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public sealed class PageAttributesSurfaceController : SurfaceController
+    {
+        [ChildActionOnly]
+        public ActionResult RenderAttributes()
+        {
+            return PartialView("~/Views/Partials/PageAttributes.cshtml", CurrentPage.GetCulture());
+        }
+    }
+}
