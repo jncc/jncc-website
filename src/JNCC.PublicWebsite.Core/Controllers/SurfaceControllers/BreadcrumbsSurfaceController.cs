524bf29b0a9c87196d7407c17869b4b1bd8cf45d
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-05 17:51:52 -0500

    Rework of breadcrumbs to use Surface Controller

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/BreadcrumbsSurfaceController.cs
@@ -0,0 +1,28 @@
+﻿using JNCC.PublicWebsite.Core.Services;
+using JNCC.PublicWebsite.Core.ViewModels;
+using System.Linq;
+using System.Web.Mvc;
+using Umbraco.Web;
+using Umbraco.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public sealed class BreadcrumbsSurfaceController : SurfaceController
+    {
+        private readonly NavigationItemService _navigationItemService = new NavigationItemService();
+
+        [ChildActionOnly]
+        public ActionResult RenderBreadcrumbs()
+        {
+            var orderedAncestors = CurrentPage.Ancestors()
+                                              .OrderBy(x => x.Level);
+            var viewModel = new BreadcrumbsViewModel()
+            {
+                Ancestors = _navigationItemService.GetViewModels(orderedAncestors),
+                CurrentPage = CurrentPage.Name
+            };
+
+            return PartialView("~/Views/Partials/Breadcrumbs.cshtml", viewModel);
+        }
+    }
+}
