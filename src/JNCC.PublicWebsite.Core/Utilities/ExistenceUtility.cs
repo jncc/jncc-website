1abad57e13ffab9fab8668622bb7efb6fb2e4ab3
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-07 11:15:30 -0600

    Added ExistenceUtility

---
+++ src/JNCC.PublicWebsite.Core/Utilities/ExistenceUtility.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JNCC.PublicWebsite.Core.Utilities
+{
+    internal static class ExistenceUtility
+    {
+        public static bool IsNullOrWhiteSpace(IHtmlString htmlString)
+        {
+            return htmlString == null || string.IsNullOrWhiteSpace(htmlString.ToHtmlString());
+        }
+        public static bool IsNullOrEmpty<T>(IEnumerable<T> enumerable)
+        {
+            return enumerable == null || enumerable.Any() == false;
+        }
+    }
+}
