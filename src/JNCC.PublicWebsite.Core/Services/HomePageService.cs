becd3fcf95a1e0ebf31e4ee43a203a0b71c2521e
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-07 15:50:55 -0600

    Wiring up Callout Cards to HomePageViewModel in service

--- src/JNCC.PublicWebsite.Core/Services/HomePageService.cs
+++ src/JNCC.PublicWebsite.Core/Services/HomePageService.cs
@@ -1,6 +1,8 @@
-using JNCC.PublicWebsite.Core.Models;
+﻿using JNCC.PublicWebsite.Core.Models;
 using JNCC.PublicWebsite.Core.Utilities;
 using JNCC.PublicWebsite.Core.ViewModels;
+using System;
+using System.Collections.Generic;
 using System.Linq;
 using Umbraco.Core;
 
@@ -19,10 +21,44 @@ namespace JNCC.PublicWebsite.Core.Services
         {
             var viewModel = new HomePageViewModel()
             {
-                Carousel = GetCarouselViewModel(content)
+                Carousel = GetCarouselViewModel(content),
+                CalloutCards = GetCalloutCards(content)
             };
 
             return viewModel;
+        }
+
+        private IEnumerable<CalloutCardViewModel> GetCalloutCards(HomePage content)
+        {
+            var viewModels = new List<CalloutCardViewModel>();
+
+            if (ExistenceUtility.IsNullOrEmpty(content.CalloutCards))
+            {
+                return viewModels;
+            }
+
+            foreach (var card in content.CalloutCards)
+            {
+                var viewModel = new CalloutCardViewModel()
+                {
+                    Title = card.Title,
+                    Content = card.Content,
+                    ReadMoreButton = _navigationItemService.GetViewModel(card.ReadMoreButton)
+                };
+
+                if (card.Image != null)
+                {
+                    viewModel.Image = new ImageViewModel()
+                    {
+                        Url = card.Image.Url,
+                        AlternativeText = card.Image.ImageAlternativeText
+                    };
+                };
+
+                viewModels.Add(viewModel);
+            }
+
+            return viewModels;
         }
 
         private CarouselViewModel GetCarouselViewModel(IPageHeroCarouselComposition content)
