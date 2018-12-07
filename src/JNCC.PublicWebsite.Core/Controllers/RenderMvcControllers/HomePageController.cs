2d114e15f8936c42a279286f8ab77b7350368c33
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-07 14:08:22 -0600

    Add controller and view changes for Carousel

---
+++ src/JNCC.PublicWebsite.Core/Controllers/RenderMvcControllers/HomePageController.cs
@@ -0,0 +1,19 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Services;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.RenderMvcControllers
+{
+    public sealed class HomePageController : RenderMvcController
+    {
+        private readonly HomePageService _homePageService = new HomePageService();
+
+        public ActionResult Index(HomePage model)
+        {
+            var viewModel = _homePageService.GetViewModel(model);
+
+            return CurrentTemplate(viewModel);
+        }
+    }
+}
