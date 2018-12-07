595f18dfaf06f965e61b681a34963be49c156300
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-07 15:48:34 -0600

    Added authoring changes for Home Page Callout Cards

---
+++ src/JNCC.PublicWebsite.Core/Models/CalloutCardSchema.cs
@@ -0,0 +1,21 @@
+﻿using JNCC.PublicWebsite.Core.Extensions;
+using Umbraco.ModelsBuilder;
+
+namespace JNCC.PublicWebsite.Core.Models
+{
+    public partial class CalloutCardSchema
+    {       
+        ///<summary>
+        /// Image: An image which illustrates this content item.
+        ///</summary>
+        [ImplementPropertyType("image")]
+        public ContentImageSchema Image
+        {
+            get
+            {
+                return this.GetPropertyValueFirstOfTypeOrDefault<ContentImageSchema>("image");
+            }
+        }
+
+    }
+}
