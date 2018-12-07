0938ab19267e9da501f20c2fa6e603a58f5c8b1e
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-07 13:54:22 -0600

    Added HomePageService to wireup view model

---
+++ src/JNCC.PublicWebsite.Core/Services/HomePageService.cs
@@ -0,0 +1,38 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Utilities;
+using JNCC.PublicWebsite.Core.ViewModels;
+using System.Linq;
+using Umbraco.Core;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal sealed class HomePageService
+    {
+        public HomePageViewModel GetViewModel(HomePage content)
+        {
+            var viewModel = new HomePageViewModel()
+            {
+                Carousel = GetCarouselViewModel(content)
+            };
+
+            return viewModel;
+        }
+
+        private CarouselViewModel GetCarouselViewModel(IPageHeroCarouselComposition content)
+        {
+            if (ExistenceUtility.IsNullOrEmpty(content.HeroImages))
+            {
+                return null;
+            }
+
+            var viewModel = new CarouselViewModel()
+            {
+                ImageUrls = content.HeroImages.WhereNotNull().Select(x => x.Url),
+                Headline = content.Headline,
+                Text = content.HeroContent
+            };
+
+            return viewModel;
+        }
+    }
+}
