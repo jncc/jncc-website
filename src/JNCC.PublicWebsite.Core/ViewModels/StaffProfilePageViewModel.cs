4a1a1591f58a2551d8d9283a79c28101da7beaec
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-07 11:06:14 -0600

    Added view model for Staff Profile Page

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/StaffProfilePageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class StaffProfilePageViewModel
+    {
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public string JobTitle { get; set; }
+        public IEnumerable<string> ProfileTags { get; set; }
+        public IReadOnlyDictionary<string, IHtmlString> TabbedContent { get; set; }
+    }
+}
