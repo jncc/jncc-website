f8b2084fbcf5aa04ebb9b1722d4480e3771cf392
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-07 13:48:51 -0600

    Added view models for home page carousel

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/CarouselViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Web;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class CarouselViewModel
+    {
+        public string Headline { get; set; }
+        public IHtmlString Text { get; set; }
+        public IEnumerable<string> ImageUrls { get; set; }
+    }
+}
---
+++ src/JNCC.PublicWebsite.Core/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class HomePageViewModel
+    {
+        public CarouselViewModel Carousel { get; set; }
+    }
+}
