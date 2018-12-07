c13e956f6ea882c10939819cd0fd2a19af0f299e
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-07 11:17:01 -0600

    Added StaffProfilePageController to wire up model to view model

---
+++ src/JNCC.PublicWebsite.Core/Controllers/RenderMvcControllers/StaffProfilePageController.cs
@@ -0,0 +1,19 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Services;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.RenderMvcControllers
+{
+    public sealed class StaffProfilePageController : RenderMvcController
+    {
+        private readonly StaffProfileService _staffProfileService = new StaffProfileService();
+
+        public ActionResult Index(StaffProfilePage model)
+        {
+            var viewModel = _staffProfileService.GetViewModel(model);
+
+            return CurrentTemplate(viewModel);
+        }
+    }
+}
