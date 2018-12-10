f7b72779b3226739b1c5519008e33fcb52375eba
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-10 15:27:45 -0600

    Added ResourceItemViewModel

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/ResourceItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class ResourceItemViewModel
+    {
+        public string ImageUrl { get; set; }
+        public IHtmlString Content { get; set; }
+        public NavigationItemViewModel ReadMoreButton { get; set; }
+    }
+}
