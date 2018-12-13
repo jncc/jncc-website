2ec0e827df2f20b89a3d6f2ef36fce827bf08e39
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-13 11:21:04 -0600

    Added MainNavigationViewModel

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/MainNavigationViewModel.cs
@@ -0,0 +1,11 @@
+﻿using JNCC.PublicWebsite.Core.Constants;
+using System.Collections.Generic;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class MainNavigationViewModel
+    {
+        public IEnumerable<MainNavigationItemViewModel> Items { get; set; }
+        public PageHeroAvailability PageHeroAvailability { get; set; }
+    }
+}
