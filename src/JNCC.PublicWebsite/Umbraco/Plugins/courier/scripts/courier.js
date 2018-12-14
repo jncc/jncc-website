7ebb9a26c1fd0a67b69247a92b404bbb2de7d634
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-14 09:43:22 -0600

    Initial commit of Umbraco Courier

---
+++ src/JNCC.PublicWebsite/Umbraco/Plugins/courier/scripts/courier.js
@@ -0,0 +1,19 @@
+﻿function AppendHelp(parentObj, url, title) {
+
+    if (title == null)
+        title = "Video tutorial";
+
+    var link = "<div class='courierHelpLink'><a target='_blank' onclick='OpenHelp(this); return false;' href='http://umbraco.com/help-and-support/video-tutorials" + url + "'>" + title + "<i class='icon icon-help-alt'></i></a>";
+    var parent = jQuery(parentObj).first();
+    parent.append(link);
+}
+
+function OpenHelp(link) {
+    //Position the popup center at the top
+    var leftPos = (screen.availWidth / 2) - (750 / 2);
+
+    var url = link.href;
+    url = url + "/tvplayer";
+    window.open(url, "TVPlayer", "left=leftPos,height=555,width=750,location=no,resizable=no,status=no,toolbar=no");
+    return false;
+}
