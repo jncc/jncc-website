f7e77b85c536fe15f394fc3e6c5b2f0021c1f988
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-13 19:45:05 -0600

    Added StaffDirectoryFilteringViewModel

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/StaffDirectoryFilteringViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class StaffDirectoryFilteringViewModel
+    {
+        public IReadOnlyDictionary<string, bool> Locations { get; set; }
+        public IReadOnlyDictionary<string, bool> Teams { get; set; }
+    }
+}
