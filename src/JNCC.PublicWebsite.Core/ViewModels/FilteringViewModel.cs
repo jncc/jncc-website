72ee8719642c361adf81bf4b270c4fe4dd853dd4
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-17 14:00:22 -0600

    Added FilteringViewModel and added to StaffDirectoryFilteringViewModel

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/FilteringViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public abstract class FilteringViewModel
+    {
+        public string SearchTerm { get; set; }
+    }
+}
--- src/JNCC.PublicWebsite.Core/ViewModels/StaffDirectoryFilteringViewModel.cs
+++ src/JNCC.PublicWebsite.Core/ViewModels/StaffDirectoryFilteringViewModel.cs
@@ -2,7 +2,7 @@
 
 namespace JNCC.PublicWebsite.Core.ViewModels
 {
-    public sealed class StaffDirectoryFilteringViewModel
+    public sealed class StaffDirectoryFilteringViewModel : FilteringViewModel
     {
         public IReadOnlyDictionary<string, bool> Locations { get; set; }
         public IReadOnlyDictionary<string, bool> Teams { get; set; }
