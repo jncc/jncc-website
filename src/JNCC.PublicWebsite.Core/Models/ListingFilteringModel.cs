36cdcc2c4ef728276ba31ba5c002211d5b41e7fc
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-17 15:00:00 -0600

    Added default of 1 for PageNumber in case it isn't model binded

--- src/JNCC.PublicWebsite.Core/Models/ListingFilteringModel.cs
+++ src/JNCC.PublicWebsite.Core/Models/ListingFilteringModel.cs
@@ -2,6 +2,11 @@
 {
     public class ListFilteringModel
     {
+        public ListFilteringModel()
+        {
+            PageNumber = 1;
+        }
+
         public string SearchTerm { get; set; }
         public int PageNumber { get; set; }
     }
