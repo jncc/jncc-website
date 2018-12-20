ef58d1ca174dba392ec2ea8ba37ca38e4c9fe7de
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-20 16:40:10 -0600

    Added initial criteria filtering to NewsAndInsightsLandingService
    Currently Year & Search term are implemented. ArticleType & Team are yet to be implemented.

--- src/JNCC.PublicWebsite.Core/Services/NewsAndInsightsLandingService.cs
+++ src/JNCC.PublicWebsite.Core/Services/NewsAndInsightsLandingService.cs
@@ -1,9 +1,13 @@
 ﻿using JNCC.PublicWebsite.Core.Constants;
 using JNCC.PublicWebsite.Core.Extensions;
 using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Utilities;
 using JNCC.PublicWebsite.Core.ViewModels;
+using System;
+using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Linq;
+using Umbraco.Core;
 using Umbraco.Web;
 
 namespace JNCC.PublicWebsite.Core.Services
@@ -22,9 +26,27 @@ namespace JNCC.PublicWebsite.Core.Services
 
         protected override IOrderedEnumerable<ArticlePage> GetOrderedChildren(NewsAndInsightsLandingPage parent, NewsAndInsightsLandingFilteringModel filteringModel)
         {
-            return parent.Children<ArticlePage>()
-                         .OrderByDescending(x => x.PublishDate)
-                         .ThenByFirstAvailableProperty(x => new string[] { x.Headline, x.Name });
+            var allChildren = parent.Children<ArticlePage>();
+            var conditions = new List<Func<ArticlePage, bool>>();
+
+            if (ExistenceUtility.IsNullOrEmpty(filteringModel.Years) == false)
+            {
+                conditions.Add(x => filteringModel.Years.Contains(x.PublishDate.Year));
+            }
+
+            if (string.IsNullOrEmpty(filteringModel.SearchTerm) == false)
+            {
+                conditions.Add(x => x.Name.InvariantContains(filteringModel.SearchTerm)
+                                 || x.Headline.InvariantContains(filteringModel.SearchTerm) 
+                                 || (ExistenceUtility.IsNullOrWhiteSpace(x.LandingPageContent) == false && x.LandingPageContent.ToString().InvariantContains(filteringModel.SearchTerm))
+                                 || (ExistenceUtility.IsNullOrWhiteSpace(x.MainContent) == false && x.MainContent.ToString().InvariantContains(filteringModel.SearchTerm))
+                              );
+            }
+
+            var actualChildren = ExistenceUtility.IsNullOrEmpty(conditions) ? allChildren : allChildren.Where(x => conditions.All(y => y.Invoke(x)));
+
+            return actualChildren.OrderByDescending(x => x.PublishDate)
+                                 .ThenByFirstAvailableProperty(x => new string[] { x.Headline, x.Name });
         }
 
         protected override ArticleListingViewModel ToViewModel(ArticlePage content)
