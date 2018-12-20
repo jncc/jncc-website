474902b1b5cf5ad25214c068f3773266bb699b31
jamiepollock <jamie.does.umbraco@gmail.com>
2018-12-20 16:12:08 -0600

    Added NewsAndInsightsLandingFilteringViewModel

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/NewsAndInsightsLandingFilteringViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class NewsAndInsightsLandingFilteringViewModel : FilteringViewModel
+    {
+        public IReadOnlyDictionary<string, bool> ArticleTypes { get; set; }
+        public IReadOnlyDictionary<string, bool> Years { get; set; }
+        public IReadOnlyDictionary<string, bool> Teams { get; set; }
+    }
+}
