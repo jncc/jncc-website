3221ec58f804fe83e35455fc1b721509edf4bb81
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-03 12:51:32 -0600

    Added UmbracoArticlePagesProvider base class

---
+++ src/JNCC.PublicWebsite.Core/Providers/UmbracoArticlePagesProvider.cs
@@ -0,0 +1,15 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using System.Collections.Generic;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace JNCC.PublicWebsite.Core.Providers
+{
+    internal abstract class UmbracoArticlePagesProvider
+    {
+        protected IEnumerable<ArticlePage> GetArticlePages(IPublishedContent root)
+        {
+            return root.Children<ArticlePage>();
+        }
+    }
+}
