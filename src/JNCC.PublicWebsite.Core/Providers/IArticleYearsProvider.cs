476a57c6f0deafa7202898760911fba369ab3916
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-03 11:22:15 -0600

    Added interfaces for ArticleYears & ArticleTypes provider

---
+++ src/JNCC.PublicWebsite.Core/Providers/IArticleYearsProvider.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace JNCC.PublicWebsite.Core.Providers
+{
+    public interface IArticleYearsProvider
+    {
+        IEnumerable<int> GetAll();
+        IEnumerable<int> GetAllDescending();
+    }
+}
