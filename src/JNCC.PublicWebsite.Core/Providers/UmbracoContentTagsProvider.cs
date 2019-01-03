b40cee8e732c27b98a9aa7cfb5a28ca4f83effc4
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-03 11:13:05 -0600

    Added ITagsProvider and UmbracoContentTagsProvider

---
+++ src/JNCC.PublicWebsite.Core/Providers/UmbracoContentTagsProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core.Services;
+
+namespace JNCC.PublicWebsite.Core.Providers
+{
+    internal sealed class UmbracoContentTagsProvider : ITagsProvider
+    {
+        private readonly ITagService _tagService;
+
+        public UmbracoContentTagsProvider(ITagService tagService)
+        {
+            _tagService = tagService;
+        }
+
+        public IEnumerable<string> GetTags(string tagGroup)
+        {
+            return _tagService.GetAllContentTags(tagGroup).OrderBy(x => x.Text)
+                                                          .Select(x => x.Text);
+        }
+    }
+}
