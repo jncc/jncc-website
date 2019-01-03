4395a9cd5d0100f4c2167bb97cf71f801c286a11
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-03 12:22:14 -0600

    Added UmbracoArticleTypesProvider

---
+++ src/JNCC.PublicWebsite.Core/Providers/UmbracoArticleTypesProvider.cs
@@ -0,0 +1,26 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Utilities;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace JNCC.PublicWebsite.Core.Providers
+{
+    internal sealed class UmbracoArticleTypesProvider : IArticleTypesProvider<IPublishedContent>
+    {
+        public IEnumerable<string> GetAllByRoot(IPublishedContent root)
+        {
+            var children = root.Children<ArticlePage>();
+
+            if (ExistenceUtility.IsNullOrEmpty(children))
+            {
+                return Enumerable.Empty<string>();
+            }
+
+            return children.Select(x => x.ArticleType)
+                           .Distinct()
+                           .OrderBy(x => x);
+        }
+    }
+}
