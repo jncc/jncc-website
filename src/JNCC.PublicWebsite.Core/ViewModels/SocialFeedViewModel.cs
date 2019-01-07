a05dfa9aad5167ff6587c2de68f7613605346f30
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-07 11:36:32 -0600

    Added SocialFeedViewModel to HomePageViewModel

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/SocialFeedViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class SocialFeedViewModel
+    {
+        public string TwitterFeedUrl { get; set; }
+        public int NumberOfTweets { get; set; }
+    }
+}
