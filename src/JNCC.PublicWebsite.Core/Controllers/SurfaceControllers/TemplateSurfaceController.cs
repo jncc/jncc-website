3aae537adce54883c0d303328db150ce2ba4d148
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-08 14:16:13 -0600

    Minor tweaks to TemplateSurfaceController

--- src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/TemplateSurfaceController.cs
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/TemplateSurfaceController.cs
@@ -1,15 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using Umbraco.Web;
 
 namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
 {
-    public class TemplateSurfaceController : CoreSurfaceController
+    public sealed class TemplateSurfaceController : CoreSurfaceController
     {
+        [ChildActionOnly]
         public ActionResult RenderTemplateName()
         {
             return Content(CurrentPage.GetTemplateAlias().ToString());
