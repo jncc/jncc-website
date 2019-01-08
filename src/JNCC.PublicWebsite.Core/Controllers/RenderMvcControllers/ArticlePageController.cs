1fed3105ba413d7eb9024ff3716f97a1000cb8ed
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-08 15:01:04 -0600

    Wired up controller and view to use new Article Page ViewModel & service

---
+++ src/JNCC.PublicWebsite.Core/Controllers/RenderMvcControllers/ArticlePageController.cs
@@ -0,0 +1,19 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Services;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.RenderMvcControllers
+{
+    public sealed class ArticlePageController : RenderMvcController
+    {
+        private readonly ArticleService _articleService = new ArticleService();
+
+        public ActionResult Index(ArticlePage model)
+        {
+            var viewModel = _articleService.GetViewModel(model);
+
+            return CurrentTemplate(viewModel);
+        }
+    }
+}
