aee987a698f13a90bf376fe36c3708e6b0b871ff
Meyers95 <ameyers@carbonsix.digital>
2019-01-08 16:59:13 +0000

    Added TemplateAlias into footer

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/TemplateSurfaceController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Umbraco.Web;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public class TemplateSurfaceController : CoreSurfaceController
+    {
+        public ActionResult RenderTemplateName()
+        {
+            return Content(CurrentPage.GetTemplateAlias().ToString());
+        }
+    }
+}
