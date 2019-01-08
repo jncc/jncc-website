2feb95e425e43ef41cc77b9867546800570ebd04
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-08 15:00:13 -0600

    Added ArticlePage view model and service

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/ArticlePageViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class ArticlePageViewModel
+    {
+        public IEnumerable<string> Teams { get; set; }
+        public string LandingPageUrl { get; set; }
+        public IHtmlString MainContent { get; set; }
+        public string ArticleType { get; set; }
+        public DateTime PublishDate { get; set; }
+    }
+}
