9a7e1f0a65d66c9cd3af58c8a9cfe9276e106aad
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-09 16:01:15 -0600

    Added surfacecontroller for rendering head and body includes.

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/PageIncludesSurfaceController.cs
@@ -0,0 +1,56 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public sealed class PageIncludesSurfaceController : CoreSurfaceController
+    {
+        [ChildActionOnly]
+        public IHtmlString RenderHeadIncludes()
+        {
+            var includesBuilder = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(Root.GlobalHeadIncludes) == false)
+            {
+                includesBuilder.Append(Root.GlobalHeadIncludes);
+            }
+
+            if (CurrentPage is IPageSpecificIncludesComposition)
+            {
+                var value = (CurrentPage as IPageSpecificIncludesComposition).PageSpecificHeadIncludes;
+
+                if (string.IsNullOrWhiteSpace(value) == false)
+                {
+                    includesBuilder.Append(value);
+                }
+            }
+
+            return new MvcHtmlString(includesBuilder.ToString());
+        }
+
+        [ChildActionOnly]
+        public IHtmlString RenderBodyIncludes()
+        {
+            var includesBuilder = new StringBuilder();
+
+            if (string.IsNullOrWhiteSpace(Root.GlobalBodyIncludes) == false)
+            {
+                includesBuilder.Append(Root.GlobalBodyIncludes);
+            }
+
+            if (CurrentPage is IPageSpecificIncludesComposition)
+            {
+                var value = (CurrentPage as IPageSpecificIncludesComposition).PageSpecificBodyIncludes;
+
+                if (string.IsNullOrWhiteSpace(value) == false)
+                {
+                    includesBuilder.Append(value);
+                }
+            }
+
+            return new MvcHtmlString(includesBuilder.ToString());
+        }
+    }
+}
