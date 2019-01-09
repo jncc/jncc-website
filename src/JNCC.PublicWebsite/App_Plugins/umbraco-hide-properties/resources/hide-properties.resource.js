3790d9d74ef56083ea123d1bea3e949209537462
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-09 16:41:09 -0600

    Added Our.Umbraco.HideProperties (v0.3.2) and custom rules
    Rules hide "Page Includes" tab from non-administrators.

---
+++ src/JNCC.PublicWebsite/App_Plugins/umbraco-hide-properties/resources/hide-properties.resource.js
@@ -0,0 +1,16 @@
+﻿function HidePropertiesResource($http) {
+    return {
+        // Rules
+        getRules: function () {
+            return $http.get(Umbraco.Sys.ServerVariables.hideProperties.getRules);
+        },      
+        saveRule: function (rule) {
+            return $http.post(Umbraco.Sys.ServerVariables.hideProperties.saveRule, rule);
+        },
+        deleteRule: function(rule){
+            return $http.delete(Umbraco.Sys.ServerVariables.hideProperties.deleteRule + "?key=" + rule.key);
+        }
+    };
+};
+
+angular.module("umbraco.resources").factory("hidePropertiesResource", HidePropertiesResource)
---
+++ src/JNCC.PublicWebsite/config/hideProperties.rules.js
@@ -0,0 +1,20 @@
+[
+  {
+    "Key": "a302b7e0-5017-4a15-9942-b7cdf463386e",
+    "IsActive": true,
+    "ContentTypeAlias": "pageSpecificIncludesComposition",
+    "Tabs": "Page Includes",
+    "Properties": "",
+    "UserGroups": "editor,translator,writer",
+    "IsDeleted": false
+  },
+  {
+    "Key": "452f2efe-d22a-491b-876c-6c82c2e7d19a",
+    "IsActive": true,
+    "ContentTypeAlias": "globalIncludesComposition",
+    "Tabs": "Page Includes",
+    "Properties": "",
+    "UserGroups": "editor,translator,writer",
+    "IsDeleted": false
+  }
+]
