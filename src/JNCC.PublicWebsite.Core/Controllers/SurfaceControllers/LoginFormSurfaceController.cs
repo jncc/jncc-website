932abfc56ae6b29c66c036301464d2c9dbe220a5
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-11 09:22:29 -0600

    Added initial wire up of Login Form Surface Controller

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/LoginFormSurfaceController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public sealed class LoginFormSurfaceController : SurfaceController
+    {
+        [HttpGet]
+        [ChildActionOnly]
+        public ActionResult RenderLoginForm()
+        {
+            return PartialView("~/Views/Partials/LoginForm.cshtml");
+        }
+
+    }
+}
