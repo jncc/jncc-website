39832170223353e6a9af21afa8b0861fe068bcf0
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-11 13:22:18 -0600

    Wired up login form surface controller & view.

--- src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/LoginFormSurfaceController.cs
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/LoginFormSurfaceController.cs
@@ -1,16 +1,62 @@
-﻿using System.Web.Mvc;
+﻿using JNCC.PublicWebsite.Core.Models;
+using System;
+using System.Web.Mvc;
+using System.Web.Security;
 using Umbraco.Web.Mvc;
 
 namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
 {
     public sealed class LoginFormSurfaceController : SurfaceController
     {
+        // Possibly add this to AppSettings or CMS eventually
+        private const int RememberMeLoginCookieDurationInMinutes = 43200;
+        private const int NonRememberMeLoginCookieDurationInMinutes = 120;
+
         [HttpGet]
         [ChildActionOnly]
         public ActionResult RenderLoginForm()
         {
-            return PartialView("~/Views/Partials/LoginForm.cshtml");
+            var model = new LoginFormModel();
+
+            return PartialView("~/Views/Partials/LoginForm.cshtml", model);
         }
 
+        [HttpPost]
+        [ChildActionOnly]
+        [ActionName("RenderLoginForm")]
+        public ActionResult RenderPostLoginForm(LoginFormModel model)
+        {
+            return PartialView("~/Views/Partials/LoginForm.cshtml", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult PerformLogin(LoginFormModel model)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            if (Members.Login(model.Username, model.Password) == false)
+            {
+                ModelState.AddModelError("Model", "The Username/Password combination provided is not a valid login.");
+                return CurrentUmbracoPage();
+            }
+
+            if (Response.Cookies[FormsAuthentication.FormsCookieName] != null)
+            {
+                var cookieDurationInMinutes = model.RememberMe ? RememberMeLoginCookieDurationInMinutes : NonRememberMeLoginCookieDurationInMinutes;
+
+                Response.Cookies[FormsAuthentication.FormsCookieName].Expires = DateTime.Now.AddMinutes(cookieDurationInMinutes);
+            }
+
+            if (Request.RawUrl == CurrentPage.Url)
+            {
+                return Redirect("/");
+            }
+
+            return RedirectToCurrentUmbracoUrl();
+        }
     }
 }
