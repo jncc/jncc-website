993b61142808f351fb6d5c1376bd4f7d52c9b662
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-16 14:51:36 -0600

    Added RenderMvcController for Change Password blocking anon users access

---
+++ src/JNCC.PublicWebsite.Core/Controllers/RenderMvcControllers/ChangePasswordPageController.cs
@@ -0,0 +1,19 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.RenderMvcControllers
+{
+    public sealed class ChangePasswordPageController : RenderMvcController
+    {
+        public ActionResult Index(ChangePasswordPage model)
+        {
+            if (Members.IsLoggedIn() == false)
+            {
+                return Redirect("/");
+            }
+
+            return CurrentTemplate(model);
+        }
+    }
+}
