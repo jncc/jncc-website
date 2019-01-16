b9de01f119a88c87c5d310ea2c03371ef4a05b26
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-16 14:49:37 -0600

    Added SurfaceController for ChangePasswordForm

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/ChangePasswordFormSurfaceController.cs
@@ -0,0 +1,63 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using System;
+using System.Web.Mvc;
+using Umbraco.Core.Logging;
+using Umbraco.Web.Models;
+using UmbConstants = Umbraco.Core.Constants;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public sealed class ChangePasswordFormSurfaceController : CoreSurfaceController
+    {
+        [HttpGet]
+        [ChildActionOnly]
+        public ActionResult RenderChangePasswordForm()
+        {
+            return PartialView("~/Views/Partials/ChangePasswordForm.cshtml", new ChangePasswordModel());
+        }
+
+        [HttpPost]
+        [ChildActionOnly]
+        [ActionName("RenderChangePasswordForm")]
+        public ActionResult RenderPostChangePasswordForm(ChangePasswordModel model)
+        {
+            return PartialView("~/Views/Partials/ChangePasswordForm.cshtml", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult PerformChangePassword(ChangePasswordModel model)
+        {
+            if (ModelState.IsValid == false)
+            {
+                return CurrentUmbracoPage();
+            }
+
+            var changingPasswordModel = new ChangingPasswordModel()
+            {
+                NewPassword = model.NewPassword,
+                OldPassword = model.OldPassword
+            };
+
+            var attempt = Members.ChangePassword(Members.CurrentUserName, changingPasswordModel, UmbConstants.Conventions.Member.UmbracoMemberProviderName);
+
+            if (attempt.Success == false)
+            {
+                if (attempt.Exception != null)
+                {
+                    ModelState.AddModelError("Model", attempt.Exception);
+                    LogHelper.Error<ChangePasswordFormSurfaceController>("Unable to change password", attempt.Exception);
+                }
+                else
+                {
+                    ModelState.AddModelError("Model", "Unable to change password at this time. Ensure your credentials are correct.");
+                }
+
+                return CurrentUmbracoPage();
+            }
+            TempData.Add("Success", true);
+
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
