45d7a0a2026d1a2c7a2ed4e2ee9423069812c5b6
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-18 16:40:27 -0600

    Added IMemberNotificationService and implementation with config changes

---
+++ src/JNCC.PublicWebsite.Core/Services/IMemberNotificationService.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Umbraco.Core.Models;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal interface IMemberNotificationService
+    {
+        void SendNotification(IMember member, IDictionary<string, object> data);
+    }
+}
---
+++ src/JNCC.PublicWebsite.Core/Services/MemberEmailNotificationService.cs
@@ -0,0 +1,55 @@
+﻿using JNCC.PublicWebsite.Core.Configuration;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Mail;
+using Umbraco.Core.IO;
+using Umbraco.Core.Models;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal sealed class MemberEmailNotificationService : IMemberNotificationService
+    {
+        private readonly IResetPasswordConfiguration _resetPasswordConfiguration;
+        private readonly IFileSystem _fileSystem;
+
+        public MemberEmailNotificationService(IResetPasswordConfiguration resetPasswordConfiguration)
+        {
+            _resetPasswordConfiguration = resetPasswordConfiguration;
+            _fileSystem = FileSystemProviderManager.Current.GetUnderlyingFileSystemProvider("emailTemplates");
+        }
+
+        public void SendNotification(IMember member, IDictionary<string, object> data)
+        {
+            var message = new MailMessage(_resetPasswordConfiguration.FromEmailAddress, member.Email)
+            {
+                IsBodyHtml = true,
+                Body = BuildTemplate(data)
+            };
+
+            using (var smtp = new SmtpClient())
+            {
+                smtp.Send(message);
+            }
+        }
+
+        private string BuildTemplate(IDictionary<string, object> data)
+        {
+            using (var stream = _fileSystem.OpenFile(_resetPasswordConfiguration.EmailTemplatePath))
+            {
+                using (var reader = new StreamReader(stream))
+                {
+                    var template = reader.ReadToEnd();
+
+                    foreach (var entry in data)
+                    {
+                        var templateKey = string.Concat("{{", entry.Key, "}}");
+
+                        template = template.Replace(templateKey, entry.Value.ToString());
+                    }
+
+                    return template;
+                }
+            }
+        }
+    }
+}
