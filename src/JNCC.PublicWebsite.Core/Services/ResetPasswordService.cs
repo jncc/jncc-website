5b90c84de7723a126dbaafdd29151a16160ad3bf
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-18 16:41:20 -0600

    Added ResetPasswordService to tie all reset password services together

---
+++ src/JNCC.PublicWebsite.Core/Services/ResetPasswordService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+using Umbraco.Web;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal sealed class ResetPasswordService
+    {
+        private readonly IResetPasswordRequestService _passwordRequestService;
+        private readonly IMemberNotificationService _memberNotificationService;
+        public ResetPasswordService(IMemberService memberService, IResetPasswordRequestService passwordRequestService, IMemberNotificationService memberNotificationService)
+        {
+            _passwordRequestService = passwordRequestService;
+            _memberNotificationService = memberNotificationService;
+        }
+
+        internal bool TrySetInitialRequest(IMember existingMember, IPublishedContent currentPage)
+        {
+            try
+            {
+                var token = _passwordRequestService.Create(existingMember.Key, DateTime.Now);
+                var data = new Dictionary<string, object>()
+                {
+                    { "Token", token },
+                    { "ResetPasswordPageUrl", currentPage.UrlWithDomain() }
+                };
+
+                _memberNotificationService.SendNotification(existingMember, data);
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                LogHelper.Error<ResetPasswordService>("Error in TrySetInitialRequest", ex);
+                return false;
+            }
+        }
+    }
+}
