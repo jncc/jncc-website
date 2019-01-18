e0673f1ed638228b24e6dac92f7847911bf04b41
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-18 16:32:25 -0600

    Added ResetPasswordRequestModel and bootstrap to create PetaPOCO table

---
+++ src/JNCC.PublicWebsite.Core/Models/ResetPasswordRequestModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.DatabaseAnnotations;
+
+namespace JNCC.PublicWebsite.Core.Models
+{
+    [TableName("ResetPasswordRequests")]
+    public sealed class ResetPasswordRequestModel
+    {
+        [PrimaryKeyColumn(AutoIncrement = false)]
+        public Guid Id { get; set; }
+        public string MemberKey { get; set; }
+        public string StatusCD { get; set; }
+        public DateTime ExpirationDate { get; set; }
+    }
+}
