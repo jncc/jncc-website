25d4084038ab1974389adf4258f1eef83c22019c
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-18 13:30:26 -0600

    Added RequestResetPasswordModel

---
+++ src/JNCC.PublicWebsite.Core/Models/RequestResetPasswordModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace JNCC.PublicWebsite.Core.Models
+{
+    public sealed class RequestResetPasswordModel
+    {
+        [Required]
+        [DisplayName("Email Address")]
+        public string EmailAddress { get; set; }
+    }
+}
