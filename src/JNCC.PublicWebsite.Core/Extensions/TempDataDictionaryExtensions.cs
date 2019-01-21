437595dda0cd5507c433cef26534323d0ed3184d
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-21 15:20:13 -0600

    Added TempDataDictionaryExtensions for "success flag"

---
+++ src/JNCC.PublicWebsite.Core/Extensions/TempDataDictionaryExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Extensions
+{
+    public static class TempDataDictionaryExtensions
+    {
+        public static void SetSuccessFlag(this TempDataDictionary tempData, string key)
+        {
+            tempData.Add(key, true);
+        }
+
+        public static bool HasSuccessFlag(this TempDataDictionary tempData, string key)
+        {
+            return tempData.ContainsKey(key) && tempData[key] != null;
+        }
+    }
+}
