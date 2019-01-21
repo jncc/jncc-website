be2861b93f3a4fcdf6a3b4635354bc7f9b40a1c9
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-21 13:35:44 -0600

    Added RequireParameterAttribute.cs

---
+++ src/JNCC.PublicWebsite.Core/Attributes/Routing/RequireParameterAttribute.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Attributes.Routing
+{
+    public sealed class RequireParameterAttribute : ActionMethodSelectorAttribute
+    {
+        public RequireParameterAttribute(string valueName)
+        {
+            ValueName = valueName;
+        }
+        public override bool IsValidForRequest(ControllerContext controllerContext, MethodInfo methodInfo)
+        {
+            return (controllerContext.HttpContext.Request[ValueName] != null);
+        }
+        public string ValueName { get; private set; }
+    }
+}
