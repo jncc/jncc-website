56585d35a65a59a7a510bda543908ab66af3f36a
Alex Meyers <ameyers@carbonsix.digital>
2019-01-21 11:39:37 +0000

    Added models, controller and service for search - Created search results view and updated search bar in header

---
+++ src/JNCC.PublicWebsite.Core/Controllers/RenderMvcControllers/SearchResultsPageController.cs
@@ -0,0 +1,51 @@
+﻿using JNCC.PublicWebsite.Core.Services;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.RenderMvcControllers
+{
+    public class SearchResultsPageController : RenderMvcController
+    {
+        private readonly SearchService _searchService;
+
+        public SearchResultsPageController()
+        {
+            _searchService = new SearchService();
+        }
+
+        public ActionResult Index(string searchTerm, int currentPage = 1)
+        {
+            // Cleanese the search term, removing HTML etc
+            searchTerm = CleanseSearchTerm(("" + Request["q"]).ToLower(CultureInfo.InvariantCulture));
+            // Tokenize the search term
+            Tokenize(searchTerm);
+            currentPage = int.TryParse(Request["p"], out int parsedInt) ? parsedInt : 1;
+
+            // Call to search ES Index
+            var searchResults = _searchService.EsGet(searchTerm, 4, (currentPage - 1) * 4);
+
+            var viewModel = _searchService.GetViewModel(searchResults, searchTerm, 4, currentPage);
+
+            return CurrentTemplate(viewModel);
+        }
+
+        // Cleanse the search term
+        public string CleanseSearchTerm(string input)
+        {
+            return Umbraco.StripHtml(input).ToString();
+        }
+
+        // Splits a string on space, except where enclosed in quotes
+        public IEnumerable<string> Tokenize(string input)
+        {
+            return Regex.Matches(input, @"[\""].+?[\""]|[^ ]+")
+                .Cast<Match>()
+                .Select(m => m.Value.Trim('\"'))
+                .ToList();
+        }
+    }
+}
---
+++ src/JNCC.PublicWebsite.Core/Models/SearchModel.cs
@@ -0,0 +1,59 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace JNCC.PublicWebsite.Core.Models
+{
+    public class SearchModel
+    {
+        [JsonProperty("hits")]
+        public Hits Hits { get; set; }
+    }
+
+    public class Source
+    {
+        [JsonProperty("site")]
+        public string Site { get; set; }
+        [JsonProperty("title")]
+        public string Title { get; set; }
+        [JsonProperty("content")]
+        public string Content { get; set; }
+        //public List<Keyword> keywords { get; set; }
+        [JsonProperty("published_date")]
+        public DateTime PublishedDate { get; set; }
+        [JsonProperty("data_type")]
+        public string DataType { get; set; }
+        //public List<DatahubKeyword> datahub_keywords { get; set; }
+    }
+
+    public class Hit
+    {
+        //public string _index { get; set; }
+        //public string _type { get; set; }
+        //public string _id { get; set; }
+        //public double _score { get; set; }
+        [JsonProperty("_source")]
+        public Source Source { get; set; }
+    }
+
+    public class Hits
+    {
+        [JsonProperty("total")]
+        public int Total { get; set; }
+        //public double max_score { get; set; }
+        [JsonProperty("hits")]
+        public List<Hit> Results { get; set; }
+    }
+
+    public class Keyword
+    {
+        public string vocab { get; set; }
+        public string value { get; set; }
+    }
+
+    public class DatahubKeyword
+    {
+        public string vocab { get; set; }
+        public string value { get; set; }
+    }
+}
