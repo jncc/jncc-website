a877faa6a8d1fc51bce3ab37ac2200c129d956aa
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-21 15:24:57 -0600

    Added default of SuccessFlags.Default to TempDataDictionaryExtensions

--- src/JNCC.PublicWebsite.Core/Extensions/TempDataDictionaryExtensions.cs
+++ src/JNCC.PublicWebsite.Core/Extensions/TempDataDictionaryExtensions.cs
@@ -1,15 +1,16 @@
-﻿using System.Web.Mvc;
+﻿using JNCC.PublicWebsite.Core.Constants;
+using System.Web.Mvc;
 
 namespace JNCC.PublicWebsite.Core.Extensions
 {
     public static class TempDataDictionaryExtensions
     {
-        public static void SetSuccessFlag(this TempDataDictionary tempData, string key)
+        public static void SetSuccessFlag(this TempDataDictionary tempData, string key = SuccessFlags.Default)
         {
             tempData.Add(key, true);
         }
 
-        public static bool HasSuccessFlag(this TempDataDictionary tempData, string key)
+        public static bool HasSuccessFlag(this TempDataDictionary tempData, string key = SuccessFlags.Default)
         {
             return tempData.ContainsKey(key) && tempData[key] != null;
         }
