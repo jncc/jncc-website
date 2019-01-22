af7af76ad864c47534a0f94b41dc11e6d850a7d6
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-22 14:03:27 -0600

    Added ResetPasswordPageController with redirect if logged in

---
+++ src/JNCC.PublicWebsite.Core/Controllers/RenderMvcControllers/ResetPasswordPageController.cs
@@ -0,0 +1,19 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.RenderMvcControllers
+{
+    public sealed class ResetPasswordPageController : RenderMvcController
+    {
+        public ActionResult Index(ResetPasswordPage model)
+        {
+            if (Members.IsLoggedIn())
+            {
+                return Redirect("/");
+            }
+
+            return CurrentTemplate(model);
+        }
+    }
+}
