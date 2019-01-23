2960567b4ee5869df3aa71933a80b9326e15fd05
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-23 16:10:13 -0600

    Added BlogCommentViewModel

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/BlogCommentViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class BlogCommentViewModel
+    {
+        public DateTime Created { get; set; }
+        public string Name { get; set; }
+        public string Comment { get; set; }
+        public string Id { get; set; }
+    }
+}
