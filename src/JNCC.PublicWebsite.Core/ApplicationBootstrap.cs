7d738106c89eb7d676fdd9cd706b1602f104f682
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-24 15:29:48 -0700

    Added delete comments from UmbracoForms when a blog post is deleted.

--- src/JNCC.PublicWebsite.Core/ApplicationBootstrap.cs
+++ src/JNCC.PublicWebsite.Core/ApplicationBootstrap.cs
@@ -1,6 +1,12 @@
-﻿using JNCC.PublicWebsite.Core.Models;
+﻿using System;
+using System.Linq;
+using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Services;
 using Umbraco.Core;
+using Umbraco.Core.Events;
+using Umbraco.Core.Models;
 using Umbraco.Core.Persistence;
+using Umbraco.Core.Services;
 
 namespace JNCC.PublicWebsite.Core
 {
@@ -9,6 +15,42 @@ namespace JNCC.PublicWebsite.Core
         protected override void ApplicationStarted(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
         {
             CreateResetPasswordRequestDatabaseTable(applicationContext);
+            DeleteBlogPostCommentWhenABlogPostIsSentToRecycleBin();
+        }
+
+        private void DeleteBlogPostCommentWhenABlogPostIsSentToRecycleBin()
+        {
+            ContentService.Trashing += ContentService_Trashing;
+        }
+
+        private void ContentService_Trashing(IContentService sender, MoveEventArgs<IContent> e)
+        {
+            var service = new UmbracoFormsBlogCommentsService();
+
+            foreach (var info in e.MoveInfoCollection)
+            {
+                var entity = info.Entity;
+
+                if (entity.ContentType.Alias != ArticulateMarkdown.ModelTypeAlias && entity.ContentType.Alias != ArticulateRichText.ModelTypeAlias)
+                {
+                    break;
+                }
+
+                var blogRoot = entity.Ancestors().FirstOrDefault(x => x.ContentType.Alias == Articulate.ModelTypeAlias);
+
+                if (blogRoot == null)
+                {
+                    break;
+                }
+                var blogCommentForm = blogRoot.GetValue<Guid>("commentsForm");
+
+                if (blogCommentForm == default(Guid))
+                {
+                    break;
+                }
+
+                service.DeleteComments(entity.Id, blogCommentForm);
+            }
         }
 
         private void CreateResetPasswordRequestDatabaseTable(ApplicationContext applicationContext)
