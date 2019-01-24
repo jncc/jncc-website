affff3ce102a49b010bf3d30e7de7b76a1bf1dbc
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-24 10:17:27 -0600

    Implemented commenting via Umbraco Forms

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/BlogUmbracoCommentsSurfaceController.cs
@@ -0,0 +1,33 @@
+﻿using JNCC.PublicWebsite.Core.Services;
+using JNCC.PublicWebsite.Core.Utilities;
+using System;
+using System.Web.Mvc;
+using Umbraco.Web;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public sealed class BlogUmbracoCommentsSurfaceController : CoreSurfaceController
+    {
+        [ChildActionOnly]
+        public ActionResult RenderCommentsList()
+        {
+            var service = new UmbracoFormsBlogCommentsService();
+
+            var formId = CurrentPage.GetPropertyValue<Guid>("commentsForm", true);
+
+            if (formId == default(Guid))
+            {
+                return EmptyResult();
+            }
+
+            var comments = service.GetComments(CurrentPage.Id, formId);
+
+            if (ExistenceUtility.IsNullOrEmpty(comments))
+            {
+                return EmptyResult();
+            }
+
+            return PartialView("~/Views/Partials/Blog/CommentsList.cshtml", comments);
+        }
+    }
+}
