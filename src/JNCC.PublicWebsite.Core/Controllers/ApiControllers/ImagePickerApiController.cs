08b1d4864f3b5ec4cfd30bf156511e1054a892da
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-31 11:22:31 -0600

    Added stub ImageApiController with Get method and ID property

---
+++ src/JNCC.PublicWebsite.Core/Controllers/ApiControllers/ImagePickerApiController.cs
@@ -0,0 +1,17 @@
+﻿using JNCC.PublicWebsite.Core.Services;
+using System.Net;
+using System.Web.Http;
+using Umbraco.Core.Models;
+using Umbraco.Web.WebApi;
+
+namespace JNCC.PublicWebsite.Core.Controllers.ApiControllers
+{
+    public sealed class ImagePickerApiController : UmbracoApiController
+    {
+        [HttpGet]
+        public IHttpActionResult Get(int id)
+        {
+            return Ok();
+        }
+    }
+}
