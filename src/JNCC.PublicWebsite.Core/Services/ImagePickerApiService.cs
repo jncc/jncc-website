ba524f99129423f294546fa13c67d8251c780e59
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-31 13:39:59 -0600

    Wired up ImageApiViewModel non-integer props in ImagePickerApiService

--- src/JNCC.PublicWebsite.Core/Services/ImagePickerApiService.cs
+++ src/JNCC.PublicWebsite.Core/Services/ImagePickerApiService.cs
@@ -1,16 +1,22 @@
-using JNCC.PublicWebsite.Core.Models;
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.ViewModels;
+using System.Collections.Generic;
 using Umbraco.Core.Models;
 using Umbraco.Web;
+using Umbraco.Web.Models;
+using static Umbraco.Core.Constants;
 
 namespace JNCC.PublicWebsite.Core.Services
 {
     internal sealed class ImagePickerApiService
     {
         private readonly UmbracoHelper _umbracoHelper;
+        private readonly MediaUrlResolverService _mediaUrlResolverService;
 
-        public ImagePickerApiService(UmbracoHelper umbracoHelper)
+        public ImagePickerApiService(UmbracoHelper umbracoHelper, MediaUrlResolverService mediaUrlResolverService)
         {
             _umbracoHelper = umbracoHelper;
+            _mediaUrlResolverService = mediaUrlResolverService;
         }
 
         public bool TryFindRoot(int id, out IPublishedContent root)
@@ -23,6 +29,47 @@ namespace JNCC.PublicWebsite.Core.Services
             }
 
             return root is Folder;
+        }
+
+        public IEnumerable<ImageApiViewModel> GetImages(IPublishedContent root)
+        {
+            foreach (var image in root.Descendants<Image>())
+            {
+                yield return GetImage(image);
+            }
+        }
+
+        private ImageApiViewModel GetImage(Image image)
+        {
+            return new ImageApiViewModel()
+            {
+                EditorName = image.WriterName,
+                FileType = image.UmbracoExtension,
+                LastEdited = image.UpdateDate,
+                Url = _mediaUrlResolverService.ResolveUrl(image.Url),
+                Crops = GetImageCrops(image)
+            };
+        }
+
+        private IDictionary<string, string> GetImageCrops(Image image)
+        {
+            var cropsData = image.GetPropertyValue<ImageCropDataSet>(Conventions.Media.File);
+            var dictionary = new Dictionary<string, string>();
+
+            if (cropsData == null)
+            {
+                return dictionary;
+            }
+
+            foreach (var crop in cropsData.Crops)
+            {
+                var url = image.GetCropUrl(crop.Alias);
+                var resolvedUrl = _mediaUrlResolverService.ResolveUrl(url);
+
+                dictionary.Add(crop.Alias, resolvedUrl);
+            }
+
+            return dictionary;
         }
     }
 }
