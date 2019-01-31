b469c09b846a46b0f8d17493024928503bf50d5a
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-31 11:31:07 -0600

    Added ImagePickerApiService with logic to try finding root
    Only folders are considered a valid root for the Image Picker API

---
+++ src/JNCC.PublicWebsite.Core/Services/ImagePickerApiService.cs
@@ -0,0 +1,28 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using Umbraco.Core.Models;
+using Umbraco.Web;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    public sealed class ImagePickerApiService
+    {
+        private readonly UmbracoHelper _umbracoHelper;
+
+        public ImagePickerApiService(UmbracoHelper umbracoHelper)
+        {
+            _umbracoHelper = umbracoHelper;
+        }
+
+        public bool TryFindRoot(int id, out IPublishedContent root)
+        {
+            root = _umbracoHelper.TypedMedia(id);
+
+            if (root == null)
+            {
+                return false;
+            }
+
+            return root is Folder;
+        }
+    }
+}
