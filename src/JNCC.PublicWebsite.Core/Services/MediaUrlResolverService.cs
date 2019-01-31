67ede672353aeca3633a9a9a37175b8b5e43d42d
jamiepollock <jamie.does.umbraco@gmail.com>
2019-01-31 13:38:52 -0600

    Added MediaUrlResolverService

---
+++ src/JNCC.PublicWebsite.Core/Services/MediaUrlResolverService.cs
@@ -0,0 +1,22 @@
+﻿namespace JNCC.PublicWebsite.Core.Services
+{
+    internal sealed class MediaUrlResolverService
+    {
+        private readonly string _leftPart;
+
+        public MediaUrlResolverService(string leftPart)
+        {
+            _leftPart = leftPart;
+        }
+
+        public string ResolveUrl(string url)
+        {
+            if (url.StartsWith("/"))
+            {
+                return string.Concat(_leftPart, url);
+            }
+
+            return url;
+        }
+    }
+}
