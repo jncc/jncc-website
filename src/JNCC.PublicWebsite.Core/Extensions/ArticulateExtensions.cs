2728a3c7f94129017be63fe50978c5795117cc97
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-05 14:44:35 -0600

    Added ArticulateExtensions to Get All Categories

---
+++ src/JNCC.PublicWebsite.Core/Extensions/ArticulateExtensions.cs
@@ -0,0 +1,18 @@
+﻿using Articulate.Models;
+using JNCC.PublicWebsite.Core.Providers;
+using System.Collections.Generic;
+using Umbraco.Core;
+using Umbraco.Core.Cache;
+
+namespace JNCC.PublicWebsite.Core.Extensions
+{
+    public static class ArticulateExtensions
+    {
+        public static IEnumerable<string> AllCategories(this IMasterModel articulate, ICacheProvider cacheProvider = null)
+        {
+            var provider = new UmbracoBlogCategoriesProvider(cacheProvider ?? ApplicationContext.Current.ApplicationCache.RequestCache);
+
+            return provider.GetAllCategoriesByRoot(articulate.BlogArchiveNode);
+        }
+    }
+}
