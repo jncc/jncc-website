5bba9c4c79a3d75de83d9ecabe1f2555ce76beb4
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-14 15:05:25 -0600

    Added JSON Utility for parsing JSON in a TryParse pattern.

---
+++ src/JNCC.PublicWebsite.Core/Utilities/JsonUtility.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+
+namespace JNCC.PublicWebsite.Core.Utilities
+{
+    internal static class JsonUtility
+    {
+        public static bool TryParseJson<T>(this string obj, out T result)
+        {
+            try
+            {
+                // Validate missing fields of object
+                JsonSerializerSettings settings = new JsonSerializerSettings();
+                settings.MissingMemberHandling = MissingMemberHandling.Error;
+
+                result = JsonConvert.DeserializeObject<T>(obj, settings);
+                return true;
+            }
+            catch (Exception)
+            {
+                result = default(T);
+                return false;
+            }
+        }
+    }
+}
