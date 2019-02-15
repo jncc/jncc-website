5df6efdacc4f4d7c6fb1838e428523d5823ec086
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-15 14:24:47 -0600

    Added ScienceDetailsPageViewModel

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/ScienceDetailsPageViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using System.Web;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class ScienceDetailsPageViewModel
+    {
+        public IHtmlString Preamble { get; set; }
+        public IEnumerable<ScienceDetailsSectionViewModel> Sections { get; set; }
+    }
+}
