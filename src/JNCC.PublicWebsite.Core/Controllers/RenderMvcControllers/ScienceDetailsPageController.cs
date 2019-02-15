99b11851fc7dd85821cff9686168fd805c904645
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-15 14:42:34 -0600

    Added controller wire up & view for Science Details Page

---
+++ src/JNCC.PublicWebsite.Core/Controllers/RenderMvcControllers/ScienceDetailsPageController.cs
@@ -0,0 +1,18 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Services;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.RenderMvcControllers
+{
+    public sealed class ScienceDetailsPageController : RenderMvcController
+    {
+        public ActionResult Index(ScienceDetailsPage model)
+        {
+            var service = new ScienceDetailsPageService();
+            var viewModel = service.GetViewModel(model);
+
+            return CurrentTemplate(viewModel);
+        }
+    }
+}
