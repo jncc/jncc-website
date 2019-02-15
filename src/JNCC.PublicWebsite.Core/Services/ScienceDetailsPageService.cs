8cdb397afc6cb3c8647572a37529aff46ba5169c
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-15 15:06:18 -0600

    Added GetSectionViewModels to ScienceDetailsPageService

--- src/JNCC.PublicWebsite.Core/Services/ScienceDetailsPageService.cs
+++ src/JNCC.PublicWebsite.Core/Services/ScienceDetailsPageService.cs
@@ -1,5 +1,7 @@
 ﻿using JNCC.PublicWebsite.Core.Models;
 using JNCC.PublicWebsite.Core.ViewModels;
+using System.Collections.Generic;
+using Umbraco.Core;
 
 namespace JNCC.PublicWebsite.Core.Services
 {
@@ -9,10 +11,38 @@ namespace JNCC.PublicWebsite.Core.Services
         {
             var viewModel = new ScienceDetailsPageViewModel()
             {
-                Preamble = model.Preamble
+                Preamble = model.Preamble,
+                Sections = GetSectionViewModels(model.MainContent)
             };
 
             return viewModel;
+        }
+
+        private IEnumerable<ScienceDetailsSectionViewModel> GetSectionViewModels(IEnumerable<ScienceDetailsSectionBaseSchema> mainContent)
+        {
+            var viewModels = new List<ScienceDetailsSectionViewModel>();
+
+            foreach (var section in mainContent)
+            {
+                ScienceDetailsSectionViewModel viewModel = null;
+
+                if (section is ScienceDetailsSectionRichTextSchema)
+                {
+                    viewModel = new ScienceDetailsRichTextSectionViewModel()
+                    {
+                        Content = (section as ScienceDetailsSectionRichTextSchema).Content
+                    };
+                }
+
+                if (viewModel != null)
+                {
+                    viewModel.Headline = section.Headline;
+                    viewModel.HtmlId = section.Headline.ToUrlSegment();
+                    viewModels.Add(viewModel);
+                }
+            }
+
+            return viewModels;
         }
     }
 }
