21945ab0e03993942478f00cb44a919a430ce13f
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-15 14:26:13 -0600

    Added basic ScienceDetailsPageService

---
+++ src/JNCC.PublicWebsite.Core/Services/ScienceDetailsPageService.cs
@@ -0,0 +1,13 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.ViewModels;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal sealed class ScienceDetailsPageService
+    {
+        public ScienceDetailsPageViewModel GetViewModel(ScienceDetailsPage model)
+        {
+            return new ScienceDetailsPageViewModel();
+        }
+    }
+}
