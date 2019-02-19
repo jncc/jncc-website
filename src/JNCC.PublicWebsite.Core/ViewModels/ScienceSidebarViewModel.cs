dcb08efe56b994a3c107aa2a679a9559fed20632
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-19 16:15:56 -0600

    Added HasCategories to ScienceSidebarViewModel

--- src/JNCC.PublicWebsite.Core/ViewModels/ScienceSidebarViewModel.cs
+++ src/JNCC.PublicWebsite.Core/ViewModels/ScienceSidebarViewModel.cs
@@ -1,9 +1,18 @@
-﻿using System.Collections.Generic;
+﻿using JNCC.PublicWebsite.Core.Utilities;
+using System.Collections.Generic;
 
 namespace JNCC.PublicWebsite.Core.ViewModels
 {
     public sealed class ScienceSidebarViewModel : BasicSidebarViewModel
     {
         public IEnumerable<MainNavigationItemViewModel> Categories { get; set; }
+
+        public bool HasCategories
+        {
+            get
+            {
+                return ExistenceUtility.IsNullOrEmpty(Categories) == false;
+            }
+        }
     }
 }
