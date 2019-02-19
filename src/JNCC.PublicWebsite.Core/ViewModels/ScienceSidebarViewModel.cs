13f22dc1dc6fa7a6615c3dda8abfc6b928507e24
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-19 16:13:04 -0600

    Added ScienceSidebarViewModel for science page sidebars

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/ScienceSidebarViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class ScienceSidebarViewModel : BasicSidebarViewModel
+    {
+        public IEnumerable<MainNavigationItemViewModel> Categories { get; set; }
+    }
+}
