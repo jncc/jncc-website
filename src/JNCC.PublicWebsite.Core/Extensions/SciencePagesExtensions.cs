f767deb7ca661dd0211c8cc4c5b4ed7ee555e9ab
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-20 10:57:48 -0600

    Added extension for GetCategorisationCharacter

---
+++ src/JNCC.PublicWebsite.Core/Extensions/SciencePagesExtensions.cs
@@ -0,0 +1,22 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using System.Linq;
+
+namespace JNCC.PublicWebsite.Core.Extensions
+{
+    internal static class SciencePagesExtensions
+    {
+        public static char GetCategorisationCharacter<T>(this T model) where T : ISciencePageCategorisationComposition, IPageHeroComposition
+        {
+            var names = new string[]
+            {
+                model.CategoryOrderingName,
+                model.Headline,
+                model.Name
+            };
+
+            var firstAvailableName = names.FirstOrDefault(x => string.IsNullOrWhiteSpace(x) == false);
+
+            return firstAvailableName.ToUpper().First();
+        }
+    }
+}
