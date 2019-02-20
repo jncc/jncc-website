8d4db9b6536ed512a50ee0e5a88051d670dfbcf4
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-20 13:23:26 -0600

    Initial wire up of RelatedCategories in ScienceCategoryPageService

--- src/JNCC.PublicWebsite.Core/Services/ScienceCategoryPageService.cs
+++ src/JNCC.PublicWebsite.Core/Services/ScienceCategoryPageService.cs
@@ -1,6 +1,7 @@
 ﻿using JNCC.PublicWebsite.Core.Extensions;
 using JNCC.PublicWebsite.Core.Models;
 using JNCC.PublicWebsite.Core.Providers;
+using JNCC.PublicWebsite.Core.Utilities;
 using JNCC.PublicWebsite.Core.ViewModels;
 using System.Collections.Generic;
 using System.Linq;
@@ -22,7 +23,8 @@ namespace JNCC.PublicWebsite.Core.Services
             {
                 Headline = scienceCategoryPage.GetHeadline(),
                 Preamble = scienceCategoryPage.Preamble,
-                CategorisedPages = GetCategorisedPages(scienceCategoryPage)
+                CategorisedPages = GetCategorisedPages(scienceCategoryPage),
+                RelatedCategories = GetRelatedCategories(scienceCategoryPage)
             };
         }
 
@@ -36,6 +38,22 @@ namespace JNCC.PublicWebsite.Core.Services
                                      Text = y.GetHeadline(),
                                      Url = y.Url
                                  }));
+        }
+        private IReadOnlyDictionary<char, IEnumerable<NavigationItemViewModel>> GetRelatedCategories(ScienceCategoryPage scienceCategoryPage)
+        {
+            if (ExistenceUtility.IsNullOrEmpty(scienceCategoryPage.RelatedCategories))
+            {
+                return null;
+            }
+
+            return scienceCategoryPage.RelatedCategories
+                        .GroupBy(x => x.GetCategorisationCharacter())
+                        .OrderBy(x => x.Key)
+                        .ToDictionary(x => x.Key, x => x.Select(y => new NavigationItemViewModel()
+                        {
+                            Text = y.GetHeadline(),
+                            Url = y.Url
+                        }));
         }
     }
 }
