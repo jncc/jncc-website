5cfdb539605940e00abd8abb9f526c61dd6048aa
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-20 10:15:58 -0600

    Added initial View Model for Science Category Page

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/ScienceCategoryPageViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Web;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class ScienceCategoryPageViewModel
+    {
+        public IReadOnlyDictionary<char, IEnumerable<NavigationItemViewModel>> CategorisedPages { get; set; }
+    }
+}
