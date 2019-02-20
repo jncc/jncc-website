204f6f98ac92605913600a3f121a8d33e0220ecf
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-20 11:08:08 -0600

    Added GetHeadline extension for PageHeroCompositions

---
+++ src/JNCC.PublicWebsite.Core/Extensions/PageHeroExtensions.cs
@@ -0,0 +1,19 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using System.Linq;
+
+namespace JNCC.PublicWebsite.Core.Extensions
+{
+    internal static class PageHeroExtensions
+    {
+        public static string GetHeadline(this IPageHeroComposition content)
+        {
+            var names = new string[]
+            {
+                content.Headline,
+                content.Name
+            };
+
+            return names.FirstOrDefault(x => string.IsNullOrWhiteSpace(x) == false);
+        }
+    }
+}
