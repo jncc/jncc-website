3ffa1e07713ad6d34f9b86f5ce0553696abb607d
jamiepollock <jamie.does.umbraco@gmail.com>
2019-02-22 10:56:28 -0600

    Added App_Start WebApiConfig

---
+++ src/JNCC.PublicWebsite.Core/App_Start/WebApiConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web.Http;
+using System.Web.Http.Cors;
+
+namespace JNCC.PublicWebsite.Core
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.EnableCors();
+        }
+    }
+}
