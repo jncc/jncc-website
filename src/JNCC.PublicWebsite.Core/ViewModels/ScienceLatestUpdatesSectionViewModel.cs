1c81c170946f585122000d2b4c9ee431bde62bb8
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-06 15:47:52 -0600

    Added AToZPageLink to ScienceLatestUpdatesSectionViewModel

--- src/JNCC.PublicWebsite.Core/ViewModels/ScienceLatestUpdatesSectionViewModel.cs
+++ src/JNCC.PublicWebsite.Core/ViewModels/ScienceLatestUpdatesSectionViewModel.cs
@@ -5,5 +5,6 @@ namespace JNCC.PublicWebsite.Core.ViewModels
     public sealed class ScienceLatestUpdatesSectionViewModel
     {
         public IEnumerable<ScienceLatestUpdatedPageItemViewModel> Pages { get; set; }
+        public NavigationItemViewModel AToZPageLink { get; set; }
     }
 }
