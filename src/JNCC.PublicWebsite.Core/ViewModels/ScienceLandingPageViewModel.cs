f9eea6e8241a83a461ea37ababd8fdd17227bc91
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-06 10:46:54 -0600

    Added basic ScienceLandingPageViewModel

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/ScienceLandingPageViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class ScienceLandingPageViewModel
+    {
+        public IEnumerable<CalloutCardViewModel> CalloutCards { get; set; }
+    }
+}
