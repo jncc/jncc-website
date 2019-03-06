e11274d7aed86495d69f16c3109693e6102cc7c9
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-06 11:01:17 -0600

    Added ScienceLandingPageController and wired up basic view

---
+++ src/JNCC.PublicWebsite.Core/Controllers/RenderMvcControllers/ScienceLandingPageController.cs
@@ -0,0 +1,24 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Services;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.RenderMvcControllers
+{
+    public sealed class ScienceLandingPageController : RenderMvcController
+    {
+        private readonly ScienceLandingPageService _scienceLandingPageService;
+
+        public ScienceLandingPageController()
+        {
+            _scienceLandingPageService = new ScienceLandingPageService();
+        }
+
+        public ActionResult Index(ScienceLandingPage model)
+        {
+            var viewModel = _scienceLandingPageService.GetViewModel(model);
+
+            return CurrentTemplate(viewModel);
+        }
+    }
+}
