70ccf0dcb77cbfaf5d4b52bb789e462f6a0bb11d
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-06 11:17:33 -0600

    Added Callout Cards to Science Landing Page schema & model

---
+++ src/JNCC.PublicWebsite.Core/Models/ScienceLandingPage.cs
@@ -0,0 +1,18 @@
+﻿using JNCC.PublicWebsite.Core.Extensions;
+using System.Collections.Generic;
+using Umbraco.ModelsBuilder;
+
+namespace JNCC.PublicWebsite.Core.Models
+{
+    public partial class ScienceLandingPage
+    {
+        ///<summary>
+        /// Callout Cards: Boxes for important information which will appear below the headline or hero content of the page.
+        ///</summary>
+        [ImplementPropertyType("calloutCards")]
+        public IEnumerable<CalloutCardSchema> CalloutCards
+        {
+            get { return this.GetPropertyValueOfTypeOrDefault<CalloutCardSchema>("calloutCards"); }
+        }
+    }
+}
