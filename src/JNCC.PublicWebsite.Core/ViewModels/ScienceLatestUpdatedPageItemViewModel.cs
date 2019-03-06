76217ac5db4fd6060f20800c3f59f43a5848e57d
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-06 14:18:46 -0600

    Added basic Science Latest Updates view models

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/ScienceLatestUpdatedPageItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class ScienceLatestUpdatedPageItemViewModel
+    {
+        public string Title { get; set; }
+        public IHtmlString Content { get; set; }
+        public NavigationItemViewModel ReadMoreLink { get; set; }
+    }
+}
