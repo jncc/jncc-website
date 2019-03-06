89afd01ab93e43f96978d9c43858669904ea11a6
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-06 11:19:12 -0600

    Added NavigationItem & CalloutCards services to ScienceLandingPage ctrlr

--- src/JNCC.PublicWebsite.Core/Controllers/RenderMvcControllers/ScienceLandingPageController.cs
+++ src/JNCC.PublicWebsite.Core/Controllers/RenderMvcControllers/ScienceLandingPageController.cs
@@ -7,10 +7,14 @@ namespace JNCC.PublicWebsite.Core.Controllers.RenderMvcControllers
 {
     public sealed class ScienceLandingPageController : RenderMvcController
     {
+        private readonly NavigationItemService _navigationItemService;
+        private readonly CalloutCardsService _calloutCardsService;
         private readonly ScienceLandingPageService _scienceLandingPageService;
 
         public ScienceLandingPageController()
         {
+            _navigationItemService = new NavigationItemService();
+            _calloutCardsService = new CalloutCardsService(_navigationItemService);
             _scienceLandingPageService = new ScienceLandingPageService();
         }
 
