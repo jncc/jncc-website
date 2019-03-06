cad00a3dddf0a8117864accb67185b05f2986aa4
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-06 11:00:44 -0600

    Added basic ScienceLandingPageService

---
+++ src/JNCC.PublicWebsite.Core/Services/ScienceLandingPageService.cs
@@ -0,0 +1,13 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.ViewModels;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal sealed class ScienceLandingPageService
+    {
+        public ScienceLandingPageViewModel GetViewModel(ScienceLandingPage model)
+        {
+            return new ScienceLandingPageViewModel();
+        }
+    }
+}
