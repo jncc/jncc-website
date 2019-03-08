c30c181387ac9e445181ba04744cd48271bd14f4
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-08 10:13:43 -0600

    CareersListItemViewModel now inherits from JobItemViewModel

--- src/JNCC.PublicWebsite.Core/ViewModels/CareersListItemViewModel.cs
+++ src/JNCC.PublicWebsite.Core/ViewModels/CareersListItemViewModel.cs
@@ -1,12 +1,8 @@
 ﻿namespace JNCC.PublicWebsite.Core.ViewModels
 {
-    public sealed class CareersListItemViewModel
+    public sealed class CareersListItemViewModel : JobItemViewModel
     {
-        public string Url { get; set; }
-        public string JobTitle { get; set; }
-        public string Grade { get; set; }
         public string Type { get; set; }
-        public string Location { get; set; }
         public string Team { get; set; }
     }
 }
