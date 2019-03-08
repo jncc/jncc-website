82da1ea9324063531e925e9125029c9a320725a3
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-08 09:23:32 -0600

    Added CareersSideBarViewModel

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/CareersSidebarViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class CareersSidebarViewModel : BasicSidebarViewModel
+    {
+        public IEnumerable<NavigationItemViewModel> LatestJobs { get; set; }
+    }
+}
