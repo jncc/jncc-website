eb6c86f0beaefc579c51845ec99bfaa4a76d66bc
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-08 09:09:01 -0600

    Added CareersLandingPageController with service GetViewModel wire up

---
+++ src/JNCC.PublicWebsite.Core/Controllers/RenderMvcControllers/CareersLandingPageController.cs
@@ -0,0 +1,19 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Services;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.RenderMvcControllers
+{
+    public sealed class CareersLandingPageController : RenderMvcController
+    {
+        private readonly CareersLandingPageService _careersLandingPageService = new CareersLandingPageService();
+
+        public ActionResult Index(CareersLandingPage model)
+        {
+            var viewModel = _careersLandingPageService.GetViewModel(model);
+
+            return CurrentTemplate(viewModel);
+        }
+    }
+}
