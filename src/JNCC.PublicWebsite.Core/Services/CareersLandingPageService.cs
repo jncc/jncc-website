a7fa7b61f0103cb60237af27c3e8d50571b9f1f0
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-08 08:49:41 -0600

    Added CareersLandingPageService with wire up for view model

---
+++ src/JNCC.PublicWebsite.Core/Services/CareersLandingPageService.cs
@@ -0,0 +1,50 @@
+﻿using JNCC.PublicWebsite.Core.Extensions;
+using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.ViewModels;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Web;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal sealed class CareersLandingPageService
+    {
+        public CareersLandingPageViewModel GetViewModel(CareersLandingPage model)
+        {
+            return new CareersLandingPageViewModel()
+            {
+                Preamble = model.Preamble,
+                MainContent = model.MainContent,
+                Careers = GetCareers(model)
+            };
+        }
+
+        private IEnumerable<CareersListItemViewModel> GetCareers(CareersLandingPage model)
+        {
+            var jobs = model.Children<IndividualJobPage>();
+            var viewModels = new List<CareersListItemViewModel>();
+
+            if (jobs.Any() == false)
+            {
+                return viewModels;
+            }
+
+            foreach (var job in jobs.OrderBy(x => x.SortOrder))
+            {
+                var viewModel = new CareersListItemViewModel()
+                {
+                    Grade = job.Grade,
+                    JobTitle = job.GetHeadline(),
+                    Location = job.Location,
+                    Team = job.Team,
+                    Type = job.TypeOfAppointment,
+                    Url = job.Url
+                };
+
+                viewModels.Add(viewModel);
+            }
+
+            return viewModels;
+        }
+    }
+}
