9db0f473a5f0a3deaa6c7130009bffb428c585ef
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-08 08:35:32 -0600

    Added view models for Careers Landing Page

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/CareersLandingPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Web;
+using Umbraco.Core.Models;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class CareersLandingPageViewModel
+    {
+        public IHtmlString Preamble { get; set; }
+        public IEnumerable<IPublishedContent> MainContent { get; set; }
+        public IEnumerable<CareersListItemViewModel> Careers { get; set; }
+    }
+}
---
+++ src/JNCC.PublicWebsite.Core/ViewModels/CareersListItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class CareersListItemViewModel
+    {
+        public string JobTitle { get; set; }
+        public string Grade { get; set; }
+        public string Type { get; set; }
+        public string Location { get; set; }
+        public string Team { get; set; }
+    }
+}
