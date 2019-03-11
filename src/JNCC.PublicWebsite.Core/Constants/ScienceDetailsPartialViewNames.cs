2fdc57cea4fab2e24670cf95c48cdc8888238b80
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-11 16:12:25 -0500

    Added constants file for Science Details Partial View Names

---
+++ src/JNCC.PublicWebsite.Core/Constants/ScienceDetailsPartialViewNames.cs
@@ -0,0 +1,8 @@
+﻿namespace JNCC.PublicWebsite.Core.Constants
+{
+    internal static class ScienceDetailsPartialViewNames
+    {
+        public const string RichText = "RichText";
+        public const string ImageGallery = "ImageGallery";
+    }
+}
