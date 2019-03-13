f1bd71315967dbb6ced82e424b09c5dcafc94611
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-13 10:12:41 -0500

    Added GenericListingPageItemViewModel

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/GenericListingPageItemViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.Web;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class GenericListingPageItemViewModel
+    {
+        public string Title { get; set; }
+        public string Url { get; set; }
+        public IHtmlString Content { get; set; }
+    }
+}
