dc2ab83c463685ba712251e7b107eb5fea151b4a
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-14 15:44:44 -0500

    Added basic ScienceAtoZPage service

---
+++ src/JNCC.PublicWebsite.Core/Services/ScienceAtoZPageService.cs
@@ -0,0 +1,17 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.ViewModels;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal sealed class ScienceAtoZPageService
+    {
+        public ScienceAtoZPageService()
+        {
+        }
+
+        public ScienceAtoZPageViewModel GetViewModel(ScienceAtoZpage model)
+        {
+            return new ScienceAtoZPageViewModel();
+        }
+    }
+}
