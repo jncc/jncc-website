b4f41c133cb38f005f02938226d950f42ccdf37e
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-14 16:30:06 -0500

    Added IScienceCategorisablePage to Science Details & Category Page model

--- src/JNCC.PublicWebsite.Core/Models/ScienceCategoryPage.cs
+++ src/JNCC.PublicWebsite.Core/Models/ScienceCategoryPage.cs
@@ -4,7 +4,7 @@ using Umbraco.ModelsBuilder;
 
 namespace JNCC.PublicWebsite.Core.Models
 {
-    public partial class ScienceCategoryPage
+    public partial class ScienceCategoryPage : IScienceCategorisablePage
     {
         ///<summary>
         /// Related Categories: Allows editors to related categories to the current category.  This is a one way process, if the linked category is also related to this one, it must be authored on the other category page too.
