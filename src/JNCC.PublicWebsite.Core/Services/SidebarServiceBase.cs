e8354a14bf424cf83c50038281b923180533f711
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-15 15:46:27 -0500

    Add GetDataHubLinks to SidebarServiceBase

--- src/JNCC.PublicWebsite.Core/Services/SidebarServiceBase.cs
+++ src/JNCC.PublicWebsite.Core/Services/SidebarServiceBase.cs
@@ -1,10 +1,14 @@
-﻿using JNCC.PublicWebsite.Core.ViewModels;
+﻿using JNCC.PublicWebsite.Core.Constants;
 using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Utilities;
+using JNCC.PublicWebsite.Core.ViewModels;
+using System.Collections.Generic;
 
 namespace JNCC.PublicWebsite.Core.Services
 {
     internal abstract class SidebarServiceBase
     {
+        private const int _maxNumberOfResults = 5;
         protected readonly NavigationItemService _navigationItemService;
         protected readonly IDataHubRawQueryService _dataHubRawQueryService;
 
@@ -19,8 +23,38 @@ namespace JNCC.PublicWebsite.Core.Services
             return new T
             {
                 PrimaryCallToActionButton = _navigationItemService.GetViewModel(composition.SidebarPrimaryCallToActionButton),
+                DataHubLinks = GetDataHubLinks(composition),
                 SeeAlsoLinks = _navigationItemService.GetViewModels(composition.SidebarSeeAlsoLinks)
             };
+        }
+
+        private IEnumerable<NavigationItemViewModel> GetDataHubLinks(ISidebarComposition composition)
+        {
+            var viewModels = new List<NavigationItemViewModel>();
+            if (string.IsNullOrWhiteSpace(composition.SidebarDataHubQuery))
+            {
+                return viewModels;
+            }
+
+            var results = _dataHubRawQueryService.GetByRawQuery(composition.SidebarDataHubQuery, _maxNumberOfResults);
+            if (ExistenceUtility.IsNullOrEmpty(results.Hits.Results))
+            {
+                return viewModels;
+            }
+
+            foreach (var result in results.Hits.Results)
+            {
+                var viewModel = new NavigationItemViewModel()
+                {
+                    Text = result.Source.Title,
+                    Url = result.Source.Url,
+                    Target = HtmlAnchorTargets.Blank
+                };
+
+                viewModels.Add(viewModel);
+            }
+
+            return viewModels;
         }
     }
 }
