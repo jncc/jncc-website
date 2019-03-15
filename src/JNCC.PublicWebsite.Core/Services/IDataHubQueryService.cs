0d92bbb3d58f23a306497b46b92c2b051dfd1a9f
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-15 14:54:45 -0500

    Added IDataHubRawQueryService

---
+++ src/JNCC.PublicWebsite.Core/Services/IDataHubQueryService.cs
@@ -0,0 +1,10 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using System.Collections.Generic;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    public interface IDataHubRawQueryService
+    {
+        IEnumerable<SearchModel> GetByRawQuery(string query, int numberOfItems);
+    }
+}
