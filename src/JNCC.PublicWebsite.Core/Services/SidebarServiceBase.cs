b577848218b55a3d207de6657d098f7d489382d9
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-15 15:36:46 -0500

    Added IDataHubRawQueryService to SidebarServiceBase constructor

--- src/JNCC.PublicWebsite.Core/Services/SidebarServiceBase.cs
+++ src/JNCC.PublicWebsite.Core/Services/SidebarServiceBase.cs
@@ -6,10 +6,12 @@ namespace JNCC.PublicWebsite.Core.Services
     internal abstract class SidebarServiceBase
     {
         protected readonly NavigationItemService _navigationItemService;
+        protected readonly IDataHubRawQueryService _dataHubRawQueryService;
 
-        public SidebarServiceBase(NavigationItemService navigationItemService)
+        public SidebarServiceBase(NavigationItemService navigationItemService, IDataHubRawQueryService dataHubRawQueryService)
         {
             _navigationItemService = navigationItemService;
+            _dataHubRawQueryService = dataHubRawQueryService;
         }
 
         protected T CreateViewModel<T>(ISidebarComposition composition) where T : BasicSidebarViewModel, new()
