dafcfd4fc8ed6557b33e6c6de01382a60df58078
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-15 15:23:59 -0500

    Added SidebarServiceBase to unify basic sidebar creation

---
+++ src/JNCC.PublicWebsite.Core/Services/SidebarServiceBase.cs
@@ -0,0 +1,24 @@
+﻿using JNCC.PublicWebsite.Core.ViewModels;
+using JNCC.PublicWebsite.Core.Models;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal abstract class SidebarServiceBase
+    {
+        protected readonly NavigationItemService _navigationItemService;
+
+        public SidebarServiceBase(NavigationItemService navigationItemService)
+        {
+            _navigationItemService = navigationItemService;
+        }
+
+        protected T CreateViewModel<T>(ISidebarComposition composition) where T : BasicSidebarViewModel, new()
+        {
+            return new T
+            {
+                PrimaryCallToActionButton = _navigationItemService.GetViewModel(composition.SidebarPrimaryCallToActionButton),
+                SeeAlsoLinks = _navigationItemService.GetViewModels(composition.SidebarSeeAlsoLinks)
+            };
+        }
+    }
+}
