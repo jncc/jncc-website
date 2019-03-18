af0a2c1c04e3a9965094d428355087c5f5d613ac
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-18 14:38:41 -0500

    Added SurfaceController & Partial View for RenderPageMetaInformation

---
+++ src/JNCC.PublicWebsite.Core/Controllers/SurfaceControllers/PageMetaInformationSurfaceController.cs
@@ -0,0 +1,22 @@
+﻿using JNCC.PublicWebsite.Core.Extensions;
+using JNCC.PublicWebsite.Core.Models;
+using System.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.SurfaceControllers
+{
+    public sealed class PageMetaInformationSurfaceController : CoreSurfaceController
+    {
+        [ChildActionOnly]
+        public ActionResult RenderPageMetaInformation()
+        {
+            if (CurrentPage is IPageMetaInformationComposition)
+            {
+                return EmptyResult();
+            }
+
+            var viewModel = (CurrentPage as IPageMetaInformationComposition).GetPublishedDateOrDefault();
+
+            return PartialView("~/Views/Partials/PageMetaInformation.cshtml", viewModel);
+        }
+    }
+}
