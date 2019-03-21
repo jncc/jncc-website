2fd7a28f8d5df4c8fe03e3fd27f254ae3bf8eb7f
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-21 15:37:55 -0500

    Added RequestThrottling constants

---
+++ src/JNCC.PublicWebsite.Core/Constants/RequestThrottling.cs
@@ -0,0 +1,8 @@
+﻿namespace JNCC.PublicWebsite.Core.Constants
+{
+    public static class RequestThrottling
+    {
+        public const int NumberOfRequests = 5;
+        public const int NumberOfSeconds = 5;
+    }
+}
