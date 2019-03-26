068fd576e15dbb226a9bb6a7f7bf2528090d6441
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-26 11:36:37 -0500

    Added iFramePageService

---
+++ src/JNCC.PublicWebsite.Core/Services/IFramePageService.cs
@@ -0,0 +1,76 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Text;
+using System.Web;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal sealed class IFramePageService
+    {
+        private readonly NavigationItemService _navigationItemService;
+
+        public IFramePageService(NavigationItemService navigationItemService)
+        {
+            _navigationItemService = navigationItemService;
+        }
+
+        public IFramePageViewModel GetViewModel(IFramePage model, Uri currentUrl)
+        {
+            return new IFramePageViewModel()
+            {
+                SourceUrl = GetSourceUrl(model, currentUrl)
+            };
+        }
+
+        private string GetSourceUrl(IFramePage model, Uri currentUrl)
+        {
+            if (model.PassthroughQuerystringParameters == false)
+            {
+                return model.SourceUrl;
+            }
+
+            var sourceUrlBuilder = new UriBuilder(model.SourceUrl);
+            var currentUrlBuilder = new UriBuilder(currentUrl);
+
+            var sourceUrlQuery = HttpUtility.ParseQueryString(sourceUrlBuilder.Query);
+            var currentUrlQuery = HttpUtility.ParseQueryString(currentUrlBuilder.Query);
+
+            sourceUrlBuilder.Query = BuildNewQueryStringFavouringCurrentUrlQueryValues(sourceUrlQuery, currentUrlQuery);
+            return sourceUrlBuilder.ToString();
+        }
+
+        private string BuildNewQueryStringFavouringCurrentUrlQueryValues(NameValueCollection sourceUrlQuery, NameValueCollection currentUrlQuery)
+        {
+            var allKeys = new List<string>();
+            allKeys.AddRange(currentUrlQuery.AllKeys);
+            allKeys.AddRange(sourceUrlQuery.AllKeys);
+
+            var newQuery = new StringBuilder();
+
+            foreach (var key in allKeys.Distinct())
+            {
+                var newValue = currentUrlQuery.Get(key);
+                var existingValue = sourceUrlQuery.Get(key);
+                var actualValue = string.Empty;
+
+                if (string.IsNullOrWhiteSpace(newValue) == false)
+                {
+                    actualValue = newValue;
+                }
+                else if (string.IsNullOrWhiteSpace(existingValue) == false)
+                {
+                    actualValue = existingValue;
+                }
+
+                newQuery.AppendFormat("{0}={1}&", key, actualValue);
+            }
+
+            return newQuery.ToString().TrimEnd('&');
+        }
+
+    }
+}
