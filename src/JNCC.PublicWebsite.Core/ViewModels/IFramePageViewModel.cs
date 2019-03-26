95d93ab9db67c27b400353e26596826d3fefe1e4
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-26 11:53:58 -0500

    IFramePageViewModel now uses MainNavigationViewModel

--- src/JNCC.PublicWebsite.Core/ViewModels/IFramePageViewModel.cs
+++ src/JNCC.PublicWebsite.Core/ViewModels/IFramePageViewModel.cs
@@ -1,10 +1,8 @@
-﻿using System.Collections.Generic;
-
-namespace JNCC.PublicWebsite.Core.ViewModels
+﻿namespace JNCC.PublicWebsite.Core.ViewModels
 {
     public sealed class IFramePageViewModel
     {
-        public IEnumerable<NavigationItemViewModel> Navigation { get; set; }
+        public MainNavigationViewModel Navigation { get; set; }
         public string SourceUrl { get; set; }
     }
 }
