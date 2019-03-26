81fa24674e4b16b3735c85b02b4aff955050e7ae
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-26 11:20:51 -0500

    Added IFrameViewModel class

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/IFrameViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class IFrameViewModel
+    {
+        public IEnumerable<NavigationItemViewModel> Navigation { get; set; }
+        public string SourceUrl { get; set; }
+    }
+}
