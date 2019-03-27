43111a23cb5796b8abb502468e2916a0663d2447
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-27 14:59:01 -0500

    Added GetSeoDescription to SeoMetaDataService

--- src/JNCC.PublicWebsite.Core/Services/SeoMetaDataService.cs
+++ src/JNCC.PublicWebsite.Core/Services/SeoMetaDataService.cs
@@ -1,5 +1,8 @@
 ﻿using JNCC.PublicWebsite.Core.Models;
 using JNCC.PublicWebsite.Core.ViewModels;
+using SEOChecker.MVC;
+using System;
+using Umbraco.Core.Logging;
 using Umbraco.Core.Models;
 
 namespace JNCC.PublicWebsite.Core.Services
@@ -30,6 +33,29 @@ namespace JNCC.PublicWebsite.Core.Services
                 Description = settings.Description,
                 Keywords = settings.Keywords
             };
+        }
+
+        public string GetSeoDescription(IPublishedContent content)
+        {
+            var description = string.Empty;
+
+            if (content is ISeoComposition == false)
+            {
+                return description;
+            }
+
+            try
+            {
+                var seoMetaData = new MetaData(content.Id);
+                description = seoMetaData.Description;
+            }
+            catch (Exception ex)
+            {
+                var message = string.Format("Unable to access SEO data for node ID {0}", content.Id);
+                LogHelper.Error<GenericListingPage>(message, ex);
+            }
+
+            return description;
         }
     }
 }
