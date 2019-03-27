3235cdce7447568c4b1f492cc54745a031da944c
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-27 15:10:18 -0500

    Changed RelatedItemViewModel content from string to IHtmlString

--- src/JNCC.PublicWebsite.Core/ViewModels/RelatedItemViewModel.cs
+++ src/JNCC.PublicWebsite.Core/ViewModels/RelatedItemViewModel.cs
@@ -1,11 +1,9 @@
-﻿using System.Web;
-
-namespace JNCC.PublicWebsite.Core.ViewModels
+﻿namespace JNCC.PublicWebsite.Core.ViewModels
 {
     public sealed class RelatedItemViewModel
     {
         public string ImageUrl { get; set; }
-        public IHtmlString Content { get; set; }
+        public string Content { get; set; }
         public NavigationItemViewModel Link { get; set; }
     }
 }
