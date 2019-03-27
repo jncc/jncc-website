21e2e0f2aca838f1d96b48038ea18c8bb3f24f46
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-27 15:45:21 -0500

    Added GetHeadline extension for generic IPublishedContent

--- src/JNCC.PublicWebsite.Core/Extensions/PageHeroExtensions.cs
+++ src/JNCC.PublicWebsite.Core/Extensions/PageHeroExtensions.cs
@@ -1,10 +1,21 @@
 ﻿using JNCC.PublicWebsite.Core.Models;
 using System.Linq;
+using Umbraco.Core.Models;
 
 namespace JNCC.PublicWebsite.Core.Extensions
 {
     internal static class PageHeroExtensions
     {
+        public static string GetHeadline(this IPublishedContent content)
+        {
+            if (content is IPageHeroComposition)
+            {
+                return GetHeadline(content as IPageHeroComposition);
+            }
+
+            return content.Name;
+        }
+
         public static string GetHeadline(this IPageHeroComposition content)
         {
             var names = new string[]
