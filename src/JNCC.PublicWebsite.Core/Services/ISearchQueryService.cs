9857791b75d05367f4c387d84996aab50afd097b
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-27 16:11:46 -0500

    Added ISearchQueryService

---
+++ src/JNCC.PublicWebsite.Core/Services/ISearchQueryService.cs
@@ -0,0 +1,12 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using System.Threading.Tasks;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal interface ISearchQueryService
+    {
+        SearchModel EsGet(string q, int size, int start);
+
+        Task<SearchModel> ESGetAsync(string q, int size, int start);
+    }
+}
