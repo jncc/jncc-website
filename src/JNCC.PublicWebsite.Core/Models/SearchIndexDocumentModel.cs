aa9f42252c227a6ec1b5c97e1cd6cf3897a98521
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-29 16:48:10 -0500

    Added SearchIndexDocumentModel

---
+++ src/JNCC.PublicWebsite.Core/Models/SearchIndexDocumentModel.cs
@@ -0,0 +1,37 @@
+﻿using JNCC.PublicWebsite.Core.Converters;
+using Newtonsoft.Json;
+using System;
+
+namespace JNCC.PublicWebsite.Core.Models
+{
+    internal sealed class SearchIndexDocumentModel
+    {
+        [JsonProperty("id")]
+        public int NodeId { get; set; }
+
+        [JsonProperty("site")]
+        public string Site { get; set; }
+
+        [JsonProperty("title")]
+        public string Title { get; set; }
+
+        [JsonProperty("content")]
+        public string Content { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("published_date")]
+        [JsonConverter(typeof(CustomDateTimeConverter))]
+        public DateTime Published { get; set; }
+
+        [JsonProperty("file_base64")]
+        public string FileBase64Encoded { get; set; }
+
+        [JsonProperty("file_extension")]
+        public string FileExtension { get; set; }
+
+        [JsonProperty("file_bytes")]
+        public long FileSizeInBytes { get; set; }
+    }
+}
