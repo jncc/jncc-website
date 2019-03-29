7fe5681e40d3153902deba7c03a4a5ac94681f97
jamiepollock <jamie.does.umbraco@gmail.com>
2019-03-29 16:55:00 -0500

    Added service interface for queuing Upsert & Delete requests

---
+++ src/JNCC.PublicWebsite.Core/Services/ISearchIndexingQueueService.cs
@@ -0,0 +1,10 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+
+namespace JNCC.PublicWebsite.Core.Services
+{
+    internal interface ISearchIndexingQueueService
+    {
+        void QueueUpsert(SearchIndexDocumentModel document);
+        void QueueDelete(SearchIndexDocumentModel document);
+    }
+}
