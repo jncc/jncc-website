6460de6affb87b248e5585e95f60978c65401ddf
Karl Butler <kbutler@carbonsix.digital>
2019-06-03 15:19:55 +0100

    Adding Page Attributes ViewModel and Updating service to populate this

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/PageAttributesViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public class PageAttributesViewModel
+    {
+        public string HTMLLangRef { get; set; }
+        public bool NoIndex { get; set; }
+        public CultureInfo LTRValue { get; internal set; }
+    }
+}
