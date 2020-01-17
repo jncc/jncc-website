b9e6870f6b03385b25521a7ff117a59dd4bbaf8b
Pete Montgomery <sumo.the.cat@gmail.com>
2020-01-17 14:01:09 +0000

    Better validation and typo-fixing for redirects

--- tools/update-redirects/Program.cs
+++ tools/update-redirects/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Web;
 using System.Xml.Linq;
 using OfficeOpenXml;
 
@@ -10,43 +12,81 @@ namespace update_redirects
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-
             string excelSheetPath = args.FirstOrDefault();
 
             if (String.IsNullOrWhiteSpace(excelSheetPath))
-            {
                 throw new Exception("Please specify path/to/redirect-list-new-website.xlsx");
-            }
-            if (!File.Exists(excelSheetPath))
-            {
-                throw new Exception("File doesn't exist at " + excelSheetPath);
-            }
 
+            if (!File.Exists(excelSheetPath))
+                throw new Exception("File doesn't exist at " + excelSheetPath);
+
+            var redirects = GetRedirects(excelSheetPath);
+            WriteMapFile(redirects);
+        }
+
+        static IEnumerable<XElement> GetRedirects(string excelSheetPath)
+        {
             using (var excelPackage = new ExcelPackage(new FileInfo(excelSheetPath)))
             {
                 // worksheet is named "final"
                 var sheet = excelPackage.Workbook.Worksheets["final"];
 
-                var redirects  = from n in Enumerable.Range(1, sheet.Dimension.End.Row)
-                                 select sheet.Cells[n, 3].Text; // redirects are in third column
+                foreach (int n in Enumerable.Range(1, sheet.Dimension.End.Row))
+                {
+                    Console.WriteLine($"Making redirect for row {n}...");
 
-                var rewriteElements = from r in redirects
-                                      select XElement.Parse(r);
+                    // parsing values as URIs will catch all manner of errors
+                    var from =  new Uri(sheet.Cells[n, 1].Text.Trim());
+                    var to   =  new Uri(sheet.Cells[n, 2].Text.Trim());
+                    
+                    // but IIS redirect maps only want the unencoded path to match on
+                    string unencodedPath = HttpUtility.UrlDecode(from.PathAndQuery);
 
-                var rewriteMapsElement = new XElement("rewriteMaps",
-                    new XElement("rewriteMap",
-                        new XAttribute("name", "JnccDefraGovSiteRedirectsList"),
-                        new XAttribute("defaultValue", ""),
-                        rewriteElements
-                    )
-                );
-
-                File.WriteAllText(
-                    "../../src/JNCC.PublicWebsite/RewriteMaps.config",
-                    rewriteMapsElement.ToString()
-                );
+                    yield return new XElement(
+                        "add",
+                        new XAttribute("key", unencodedPath),
+                        new XAttribute("value", to.ToString())
+                    );
+                }
             }
         }
+
+        static void WriteMapFile(IEnumerable<XElement> redirects)
+        {
+            // get groups of up to 2000 redirects
+            // to try to stay below the maximum map file size!
+            // also, IIS will break if there any duplicate map keys
+            var distinctRedirects = redirects
+                .DistinctBy(r => r.Attribute("key").Value);
+
+            var rewriteMapsElement = new XElement("rewriteMaps",
+                new XElement("rewriteMap",
+                    new XAttribute("name", $"JnccDefraGovSiteRedirectsList"),
+                    new XAttribute("defaultValue", ""),
+                    distinctRedirects
+                )
+            );
+
+            string path = "../../src/JNCC.PublicWebsite/RewriteMaps.config";
+            Console.WriteLine($"Writing {path}...");
+            File.WriteAllText(path, rewriteMapsElement.ToString());
+        }
+    }
+
+    public static class Extensions
+    {
+        public static IEnumerable<TSource> DistinctBy<TSource, TKey>
+            (this IEnumerable<TSource> source, Func<TSource, TKey> keySelector)
+        {
+            var seenKeys = new HashSet<TKey>();
+
+            foreach (TSource element in source)
+            {
+                if (seenKeys.Add(keySelector(element)))
+                {
+                    yield return element;
+                }
+            }
+        }        
     }
 }
