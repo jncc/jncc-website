6d7a8297e58bd2ef03fffcd11bd04126c3eda00c
Karl Butler <kbutler@orcare.com>
2020-02-26 14:40:28 +0000

    Adding View models for IFrame Stacked Content

--- src/JNCC.PublicWebsite.Core/ViewModels/IFramePageViewModel.cs
+++ src/JNCC.PublicWebsite.Core/ViewModels/IFramePageViewModel.cs
@@ -1,4 +1,7 @@
-﻿using System.Web;
+﻿using JNCC.PublicWebsite.Core.Models;
+using JNCC.PublicWebsite.Core.Utilities;
+using System.Collections.Generic;
+using System.Web;
 
 namespace JNCC.PublicWebsite.Core.ViewModels
 {
@@ -7,5 +10,19 @@ namespace JNCC.PublicWebsite.Core.ViewModels
         public MainNavigationViewModel Navigation { get; set; }
         public string SourceUrl { get; set; }
         public IHtmlString CookieError { get; set; }
+        public string Headline { get; set; }
+        public IHtmlString Preamble { get; set; }
+        public IEnumerable<ScienceIFrameSectionViewModel> Sections { get; set; }
+        public bool HasSections
+        {
+            get
+            {
+                return ExistenceUtility.IsNullOrEmpty(Sections) == false;
+            }
+        }
+        public bool showOnMedium { get; set; }
+        public bool showOnSmall { get; set; }
+        public IFramePage Content { get; set; }
+
     }
 }
---
+++ src/JNCC.PublicWebsite.Core/ViewModels/ScienceIFrameRichTextSectionViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Web;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public sealed class ScienceIFrameRichTextSectionViewModel : ScienceIFrameSectionViewModel, IScienceIFrameRichTextSectionViewModel
+    {
+        public IHtmlString Content { get; set; }
+    }
+}
