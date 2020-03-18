41a10469cdd5e4a08ffd33e4234d69b430a91465
Karl Butler <kbutler@orcare.com>
2020-03-18 09:47:59 +0000

    Adding viewmodels for Image code component

---
+++ src/JNCC.PublicWebsite.Core/ViewModels/IScienceDetailsImageCodeSectionViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Web;
+
+namespace JNCC.PublicWebsite.Core.ViewModels
+{
+    public interface IScienceDetailsImageCodeSectionViewModel
+    {
+        string ImageCode { get; set; }
+
+        string ImagePosition { get; set; }
+
+        IHtmlString Content { get; set; }
+    }
+}
