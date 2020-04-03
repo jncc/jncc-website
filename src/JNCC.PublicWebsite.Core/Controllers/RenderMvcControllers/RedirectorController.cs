c985ab2a531db58a8246efc64f0132ae3ae563f5
Alex Meyers <ameyers@carbonsix.digital>
2020-04-03 12:56:33 +0100

    Added redirector and controller

---
+++ src/JNCC.PublicWebsite.Core/Controllers/RenderMvcControllers/RedirectorController.cs
@@ -0,0 +1,19 @@
+﻿using JNCC.PublicWebsite.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace JNCC.PublicWebsite.Core.Controllers.RenderMvcControllers
+{
+    public sealed class RedirectorController : RenderMvcController
+    {
+        public ActionResult Index(Redirector model)
+        {
+            return RedirectPermanent(model.Redirect.Url);
+        }
+    }
+}
