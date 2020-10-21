1b12edfeeb5526bfb576dbf90deaac336a0fa358
Pete Montgomery <sumo.the.cat@gmail.com>
2020-10-21 12:22:42 +0100

    Ensure own-property links are HTTPS in search results

---
+++ src/JNCC.PublicWebsite.Core/Utilities/LinkUtility.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace JNCC.PublicWebsite.Core.Utilities
+{
+    public static class LinkUtility
+    {
+        public static string EnsureHttpsForJnccLinks(string url)
+        {
+            if (Uri.TryCreate(url, UriKind.Absolute, out Uri uri))
+            {
+                if (uri.Host.Contains("jncc.gov.uk"))
+                {
+                    var b = new UriBuilder(uri)
+                    {
+                        Scheme = Uri.UriSchemeHttps,
+                        Port = -1 // default port for scheme
+                    };
+
+                    return b.ToString();
+                }
+            }
+
+            return url;
+        }
+    }
+}
