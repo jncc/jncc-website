51b330bcc692ba157f243e279195969b3d01f56a
Jonathon Cove <jcove@carbonsix.digital>
2024-02-06 15:19:56 +0000

    Added controller and classes to export pages as CSV

---
+++ src/JNCC.PublicWebsite.Core/Controllers/ApiControllers/PageExportApiController.cs
@@ -0,0 +1,70 @@
+﻿using JNCC.PublicWebsite.Core.Attributes.WebApi;
+using JNCC.PublicWebsite.Core.Services;
+using SEOChecker.Core.Extensions.UmbracoExtensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Http;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Core.Services;
+using Umbraco.Web.WebApi;
+
+namespace JNCC.PublicWebsite.Core.Controllers.ApiControllers
+{
+    //[UmbracoAuthorize]
+    public sealed class PageExportApiController : UmbracoApiController
+    {
+		[HttpGet]
+        public IHttpActionResult Export()
+        {
+			IContentService _contentService = Services.ContentService;
+            IUserService _userService = Services.UserService;
+
+            var pages = _contentService.GetRootContent().SelectRecursive(c => c.Descendants());
+            var url = HttpContext.Current.Request.Url.Host;
+
+            var formatted = pages.Select(p => new PageExportModel(p, _userService, url));
+
+            CSVExportService.GenericExport(formatted.ToArray(), "Pages", true);
+
+            return Ok("Hello");
+        }
+    }
+
+	/*
+    a csv file of all unpublished/draft page titles (or ALL pages if easier)
+    published status (if extracting all pages, I don't expect there's a difference between draft and unpublished)
+    page url (slug if easier)
+    author
+    date created
+    template used (if possible)
+    */
+	
+    public class PageExportModel
+    {
+        public PageExportModel(IContent model, IUserService userService, string url)
+        {
+            Id = model.Id;
+            Name = model.Name;
+            PublishedStatus = model.Status.ToString();
+            FrontendUrl = model.GetUrlName();
+            BackofficeUrl = url + "/umbraco#/content/content/edit/" + model.Id;
+            Author = model.GetCreatorProfile(userService)?.Name;
+            DateCreated = model.CreateDate.ToString("dd/MM/yyyy HH:mm");
+            TemplateUsed = model.Template.Name;
+		}
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string PublishedStatus { get; set; }
+        public string FrontendUrl { get; set; }
+        public string BackofficeUrl { get; set; }   
+        public string Author { get; set; }
+        public string DateCreated { get; set; }
+        public string TemplateUsed { get; set; }
+	}
+
+}
---
+++ src/JNCC.PublicWebsite.Core/Services/Interfaces/ICSVExportService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JNCC.PublicWebsite.Core.Services.Interfaces
+{
+	public interface ICSVExportService
+	{
+		void GenericExport(object[] dList, string FileName, bool SpaceCapitals = false);
+		string GenerateCSVData(object[] dList, bool SpaceCapitals = false);
+	}
+}
